ac4c4455bbf4eb350b72fbb9d40a6ad7529669ba
Jesse Jaggars <jhjaggars@gmail.com>
2021-05-18 12:49:40 -0400

    pulling db config from clowder
    Signed-off-by: Jesse Jaggars <jhjaggars@gmail.com>

--- config/config.go
+++ config/config.go
@@ -3,7 +3,7 @@ package config
 import (
 	"os"
 
-	// clowder "github.com/redhatinsights/app-common-go/pkg/api/v1"
+	clowder "github.com/redhatinsights/app-common-go/pkg/api/v1"
 
 	"github.com/spf13/viper"
 )
@@ -18,27 +18,44 @@ type EdgeConfig struct {
 	LogLevel    string
 	Debug       bool
 	UseClowder  bool
+	Database    *dbConfig
+}
+
+type dbConfig struct {
+	User     string
+	Password string
+	Hostname string
+	Port     uint
+	Name     string
 }
 
 // Get returns an initialized EdgeConfig
 func Get() *EdgeConfig {
 
-	options := viper.New()
+	var dbCfg *dbConfig
 
-	// if os.Getenv("CLOWDER_ENABLED") == "true" {
-	// 	cfg := clowder.LoadedConfig
-	// 	options.SetDefault("WebPort", cfg.WebPort)
-	// 	options.SetDefault("MetricsPort", cfg.MetricsPort)
-	// 	options.SetDefault("LogGroup", cfg.Logging.Cloudwatch.LogGroup)
-	// } else {
+	options := viper.New()
 	options.SetDefault("WebPort", 3000)
 	options.SetDefault("MetricsPort", 8080)
 	options.SetDefault("LogGroup", "platform-dev")
-	// }
-
 	options.SetDefault("LogLevel", "INFO")
 	options.SetDefault("Auth", false)
 	options.SetDefault("Debug", false)
+
+	if clowder.IsClowderEnabled() {
+		cfg := clowder.LoadedConfig
+		options.SetDefault("WebPort", cfg.PublicPort)
+		options.SetDefault("MetricsPort", cfg.MetricsPort)
+		options.SetDefault("LogGroup", cfg.Logging.Cloudwatch.LogGroup)
+		dbCfg = &dbConfig{
+			User:     cfg.Database.Username,
+			Password: cfg.Database.Password,
+			Hostname: cfg.Database.Hostname,
+			Port:     uint(cfg.Database.Port),
+			Name:     cfg.Database.Name,
+		}
+	}
+
 	options.AutomaticEnv()
 	kubenv := viper.New()
 	kubenv.AutomaticEnv()
@@ -52,5 +69,6 @@ func Get() *EdgeConfig {
 		LogGroup:    options.GetString("LogGroup"),
 		LogLevel:    options.GetString("LogLevel"),
 		UseClowder:  os.Getenv("CLOWDER_ENABLED") == "true",
+		Database:    dbCfg,
 	}
 }
--- pkg/db/db.go
+++ pkg/db/db.go
@@ -1,6 +1,10 @@
 package db
 
 import (
+	"fmt"
+
+	"github.com/redhatinsights/edge-api/config"
+	"gorm.io/driver/postgres"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -9,7 +13,23 @@ var DB *gorm.DB
 
 func InitDB() {
 	var err error
-	DB, err = gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	var dia gorm.Dialector
+	cfg := config.Get()
+
+	if cfg.Database != nil {
+		dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=enable",
+			cfg.Database.Hostname,
+			cfg.Database.User,
+			cfg.Database.Password,
+			cfg.Database.Name,
+			cfg.Database.Port,
+		)
+		dia = postgres.Open(dsn)
+	} else {
+		dia = sqlite.Open("test.db")
+	}
+
+	DB, err = gorm.Open(dia, &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
