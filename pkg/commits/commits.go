2b5af00ca224ef2306af58b1b565d54e0f8926e5
Jesse Jaggars <jhjaggars@gmail.com>
2021-05-19 10:38:11 -0400

    adding tenancy checks to commit endpoints
    Signed-off-by: Jesse Jaggars <jhjaggars@gmail.com>

--- pkg/commits/commits.go
+++ pkg/commits/commits.go
@@ -3,18 +3,21 @@ package commits
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"strconv"
 
 	"github.com/go-chi/chi"
 
 	"github.com/redhatinsights/edge-api/pkg/db"
+	"github.com/redhatinsights/platform-go-middlewares/identity"
 	"gorm.io/gorm"
 )
 
 type Commit struct {
 	gorm.Model
 	Name          string
+	Account       string
 	Hash          string
 	BuildDate     string
 	BuildNumber   uint
@@ -39,13 +42,18 @@ const commitKey key = 0
 func CommitCtx(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		var commit Commit
+		account, err := getAccount(r)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
 		if commitId := chi.URLParam(r, "commitId"); commitId != "" {
 			id, err := strconv.Atoi(commitId)
 			if err != nil {
 				http.Error(w, err.Error(), http.StatusBadRequest)
 				return
 			}
-			result := db.DB.First(&commit, id)
+			result := db.DB.Where("account = ?", account).First(&commit, id)
 			if result.Error != nil {
 				http.Error(w, result.Error.Error(), http.StatusNotFound)
 				return
@@ -56,21 +64,41 @@ func CommitCtx(next http.Handler) http.Handler {
 	})
 }
 
+func getAccount(r *http.Request) (string, error) {
+	ident := identity.Get(r.Context())
+	if ident.Identity.AccountNumber != "" {
+		return ident.Identity.AccountNumber, nil
+	}
+	return "", fmt.Errorf("cannot find account number")
+
+}
+
 func Add(w http.ResponseWriter, r *http.Request) {
 
 	var commit Commit
-	if err := json.NewDecoder(r.Body).Decode(&commit); err != nil {
+	var err error
+	if err = json.NewDecoder(r.Body).Decode(&commit); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	// more code goes here to make sure we don't fuck up
+
+	commit.Account, err = getAccount(r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 
 	db.DB.Create(&commit)
 }
 
 func GetAll(w http.ResponseWriter, r *http.Request) {
 	var commits []Commit
-	result := db.DB.Find(&commits)
+	account, err := getAccount(r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	result := db.DB.Where("account = ?", account).Find(&commits)
 	if result.Error != nil {
 		http.Error(w, result.Error.Error(), http.StatusBadRequest)
 		return
