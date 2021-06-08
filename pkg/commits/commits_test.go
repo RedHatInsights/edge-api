36eaa91d80ab9bc89ecedfa84a90a1b4e130a03a
Ana Carolina Ferreira <acosferreira@anas-air.lan>
2021-06-08 14:30:06 -0700

    add get all tests with null and empty

--- pkg/commits/commits_test.go
+++ pkg/commits/commits_test.go
@@ -1,6 +1,14 @@
 package commits
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/redhatinsights/edge-api/config"
+	"github.com/redhatinsights/edge-api/pkg/db"
+)
 
 func TestPatch(t *testing.T) {
 	commitOne := &Commit{
@@ -15,4 +23,56 @@ func TestPatch(t *testing.T) {
 	if commitOne.OSTreeRef != "two" {
 		t.Errorf("expected two got %s", commitOne.OSTreeRef)
 	}
+}
+
+func TestGetAllEmpty(t *testing.T) {
+	config.Init()
+	db.InitDB()
+	err := db.DB.AutoMigrate(Commit{})
+	if err != nil {
+		panic(err)
+	}
+	t.Run("returns empty commits", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/", nil)
+		response := httptest.NewRecorder()
+
+		GetAll(response, request)
+
+		got := response.Body.String()
+		want := "[]\n"
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+
+}
+
+func TestGetAll(t *testing.T) {
+	config.Init()
+	config.Get().Debug = true
+
+	db.InitDB()
+	err := db.DB.AutoMigrate(Commit{})
+	if err != nil {
+		panic(err)
+	}
+
+	var cmt Commit
+	cmt.Account = "0000000"
+	cmt.Name = "Test"
+	db.DB.Create(&cmt)
+
+	t.Run("returns Get all commits", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/", nil)
+		response := httptest.NewRecorder()
+
+		GetAll(response, request)
+		got := response.Body.String()
+		if !strings.Contains(got, "0000000") {
+			t.Errorf("got %q", got)
+		}
+	})
+	db.DB.Delete(&cmt)
+
 }
