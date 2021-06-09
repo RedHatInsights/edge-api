cde82dfa4537820252ebd6303179278f6fc47230
Ana Carolina Ferreira <acosferreira@anas-air.lan>
2021-06-09 15:37:32 -0700

    add test scenarios to getCommit, serveRepo

--- pkg/commits/commits_test.go
+++ pkg/commits/commits_test.go
@@ -1,6 +1,7 @@
 package commits
 
 import (
+	"context"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -71,6 +72,137 @@ func TestGetAll(t *testing.T) {
 		GetAll(response, request)
 		got := response.Body.String()
 		if !strings.Contains(got, "0000000") {
+			db.DB.Delete(&cmt)
+			t.Errorf("got %q", got)
+		}
+	})
+	db.DB.Delete(&cmt)
+}
+
+func TestGetById(t *testing.T) {
+	config.Init()
+	config.Get().Debug = true
+
+	db.InitDB()
+	err := db.DB.AutoMigrate(Commit{})
+	if err != nil {
+		panic(err)
+	}
+
+	var cmt Commit
+	cmt.Account = "0000000"
+	cmt.Name = "Test"
+	db.DB.Create(&cmt)
+
+	t.Run("returns Get commit by id", func(t *testing.T) {
+
+		request, _ := http.NewRequest(http.MethodGet, "/", nil)
+		response := httptest.NewRecorder()
+		ctx := request.Context()
+		ctx = context.WithValue(ctx, commitKey, &cmt)
+		request = request.WithContext(ctx)
+		GetByID(response, request)
+		got := response.Body.String()
+		if !strings.Contains(got, "0000000") {
+			db.DB.Delete(&cmt)
+			t.Errorf("got %q", got)
+		}
+	})
+	db.DB.Delete(&cmt)
+}
+
+func TestGetByIdFail(t *testing.T) {
+	config.Init()
+	config.Get().Debug = true
+
+	db.InitDB()
+	err := db.DB.AutoMigrate(Commit{})
+	if err != nil {
+		panic(err)
+	}
+
+	var cmt Commit
+	cmt.Account = "0000000"
+	cmt.Name = "Test"
+	db.DB.Create(&cmt)
+
+	t.Run("returns Error", func(t *testing.T) {
+
+		request, _ := http.NewRequest(http.MethodGet, "/", nil)
+		response := httptest.NewRecorder()
+
+		GetByID(response, request)
+		got := response.Body.String()
+		want := "must pass id\n"
+		if got != want {
+			db.DB.Delete(&cmt)
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+	db.DB.Delete(&cmt)
+}
+
+func TestGetCommit(t *testing.T) {
+	config.Init()
+	config.Get().Debug = true
+
+	db.InitDB()
+	err := db.DB.AutoMigrate(Commit{})
+	if err != nil {
+		panic(err)
+	}
+
+	var cmt Commit
+	cmt.Account = "0000000"
+	cmt.Name = "Test"
+	db.DB.Create(&cmt)
+
+	t.Run("returns Get commit ", func(t *testing.T) {
+
+		request, _ := http.NewRequest(http.MethodGet, "/", nil)
+		response := httptest.NewRecorder()
+		ctx := request.Context()
+		ctx = context.WithValue(ctx, commitKey, &cmt)
+		request = request.WithContext(ctx)
+		getCommit(response, request)
+		got := response.Code
+		want := http.StatusOK
+		if got != want {
+			db.DB.Delete(&cmt)
+			t.Errorf("got %q", got)
+		}
+	})
+	db.DB.Delete(&cmt)
+}
+
+func TestServeRepo(t *testing.T) {
+	config.Init()
+	config.Get().Debug = true
+
+	db.InitDB()
+	err := db.DB.AutoMigrate(Commit{})
+	if err != nil {
+		panic(err)
+	}
+
+	var cmt Commit
+	cmt.Account = "0000000"
+	cmt.Name = "Test"
+	db.DB.Create(&cmt)
+
+	t.Run("returns Get commit ", func(t *testing.T) {
+
+		request, _ := http.NewRequest(http.MethodGet, "/repo", nil)
+		response := httptest.NewRecorder()
+		ctx := request.Context()
+		ctx = context.WithValue(ctx, commitKey, &cmt)
+
+		request = request.WithContext(ctx)
+
+		ServeRepo(response, request)
+		got := response.Code
+		want := http.StatusOK
+		if got != want {
 			db.DB.Delete(&cmt)
 			t.Errorf("got %q", got)
 		}
