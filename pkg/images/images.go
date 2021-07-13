37b2d358c931008d5254fb0ed5c72ea73a1bef68
Bianca Rosa <brosa@redhat.com>
2021-07-13 18:28:42 -0300

    Create installer before compose installer

--- pkg/images/images.go
+++ pkg/images/images.go
@@ -205,11 +205,6 @@ func Create(w http.ResponseWriter, r *http.Request) {
 
 		// TODO: We need to discuss this whole thist post-July deliverable
 		if i.ImageType == models.ImageTypeInstaller {
-			i, err := imagebuilder.Client.ComposeInstaller(update, i, headers)
-			if err != nil {
-				log.Error(err)
-				return
-			}
 			i.Installer = &models.Installer{
 				Status:  models.ImageStatusBuilding,
 				Account: i.Account,
@@ -221,6 +216,11 @@ func Create(w http.ResponseWriter, r *http.Request) {
 				return
 			}
 			tx = db.DB.Save(&i.Installer)
+			i, err := imagebuilder.Client.ComposeInstaller(update, i, headers)
+			if err != nil {
+				log.Error(err)
+				return
+			}
 			if tx.Error != nil {
 				log.Error(err)
 				json.NewEncoder(w).Encode(&err)
