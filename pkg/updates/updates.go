efe5497b247275b1ebfe48b5c5e5c1efef02e5d0
acosferreira <acosferreira@gmail.com>
2021-07-21 16:06:48 -0700

    Playbook template (#98)
    * init diff
    
    * notes and initial code structure for POST
    
    Signed-off-by: Adam Miller <admiller@redhat.com>
    
    * Revert "Update entry point ostree"
    
    * init write playbook dispatcher yaml
    
    * rename template; write file output
    
    * fix lint
    
    * adjust template names
    
    * fix test
    
    * add templates to be generated on update process
    
    * init upload to s3
    
    * adjust path and fix vet
    
    * add upload to s3, remove old file, adjust path
    
    * fix PR
    
    * fix vet
    
    Co-authored-by: Ana Carolina Ferreira <acosferreira@Anas-MacBook-Air.local>
    Co-authored-by: Adam Miller <admiller@redhat.com>
    Co-authored-by: Ana Carolina Ferreira <acosferreira@anas-air.lan>

--- pkg/updates/updates.go
+++ pkg/updates/updates.go
@@ -14,6 +14,7 @@ import (
 	"github.com/redhatinsights/edge-api/pkg/common"
 	"github.com/redhatinsights/edge-api/pkg/db"
 	"github.com/redhatinsights/edge-api/pkg/models"
+	"github.com/redhatinsights/edge-api/pkg/playbooks"
 
 	apierrors "github.com/redhatinsights/edge-api/pkg/errors"
 	log "github.com/sirupsen/logrus"
@@ -103,6 +104,7 @@ func updateFromHTTP(w http.ResponseWriter, r *http.Request) (*models.UpdateTrans
 	update.Commit, err = common.GetCommitByID(updateJSON.CommitID)
 	update.Repo = &models.Repo{}
 	update.Repo.Commit = update.Commit
+
 	log.Debugf("updateFromHTTP::update.Commit: %#v", update.Commit)
 	if err != nil {
 		err := apierrors.NewInternalServerError()
@@ -110,6 +112,21 @@ func updateFromHTTP(w http.ResponseWriter, r *http.Request) (*models.UpdateTrans
 		w.WriteHeader(err.Status)
 		return &models.UpdateTransaction{}, err
 	}
+
+	var remoteInfo playbooks.TemplateRemoteInfo
+	remoteInfo.RemoteURL = update.Repo.URL
+	remoteInfo.RemoteName = update.Repo.Commit.Name
+	remoteInfo.ContentURL = update.Repo.URL
+	remoteInfo.UpdateTransaction = int(update.ID)
+	//FIX ME Add repoURL To Dispatcher Record (@Adam)
+	repoURL, err := playbooks.WriteTemplate(remoteInfo)
+	log.Debugf("playbooks:WriteTemplate: %#v", repoURL)
+	if err != nil {
+		err := apierrors.NewInternalServerError()
+		err.Title = "Error during playbook creation"
+		w.WriteHeader(err.Status)
+	}
+
 	inventoryHosts := update.InventoryHosts
 	oldCommits := update.OldCommits
 	// - populate the update.InventoryHosts []Device data
