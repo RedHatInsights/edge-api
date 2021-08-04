1583102cb84123ba02eac76baa07cb5f2fbdc6fa
Ana Carolina Ferreira <acosferreira@Anas-MacBook-Air.local>
2021-08-04 13:36:34 -0700

    adjust authorization; fix panic

--- pkg/playbooks/playbook_dispatcher.go
+++ pkg/playbooks/playbook_dispatcher.go
@@ -32,7 +32,7 @@ func ExecuteDispatcher(payload DispatcherPayload) (string, error) {
 
 	headers := common.GetOutgoingHeaders(req)
 	log.Infof("ExecuteDispatcher:: cfg.PlaybookDispatcherConfig:: %#v", cfg.PlaybookDispatcherConfig)
-	req.Header.Add("PSK", cfg.PlaybookDispatcherConfig.PSK)
+	req.Header.Add("Authorization", "PSK "+cfg.PlaybookDispatcherConfig.PSK)
 	for key, value := range headers {
 		log.Infof("Playbook dispatcher headers: %#v, %#v", key, value)
 		req.Header.Add(key, value)
@@ -42,8 +42,9 @@ func ExecuteDispatcher(payload DispatcherPayload) (string, error) {
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		log.Errorf("Error Code:: Playbook dispatcher: %#v", resp.StatusCode)
+		log.Errorf("Error Code:: Playbook dispatcher: %#v", resp)
 		log.Errorf("Error:: Playbook dispatcher: %#v", err)
+		log.Errorf("Error:: Playbook dispatcher: %#v", err.Error())
 		return models.DispatchRecordStatusError, err
 	}
 
