08e402b39cb9eb3b0529d62bca9218872b4679e3
Bianca Rosa <brosa@redhat.com>
2021-09-01 16:14:01 -0300

    Refactor devices routes to be on a separate file

--- main.go
+++ main.go
@@ -89,6 +89,7 @@ func main() {
 		s.Route("/commits", routes.MakeCommitsRouter)
 		s.Route("/images", routes.MakeImagesRouter)
 		s.Route("/updates", routes.MakeUpdatesRouter)
+		s.Route("/devices", routes.MakeDevicesRouter)
 	})
 
 	mr := chi.NewRouter()
