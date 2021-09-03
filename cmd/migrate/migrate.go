44133f6bf55ecbac620ce405686137b1a1d41793
Ana Carolina Ferreira <acosferreira@anas-air.lan>
2021-09-03 08:54:35 -0700

    add image set to auto migrate

--- cmd/migrate/migrate.go
+++ cmd/migrate/migrate.go
@@ -24,7 +24,7 @@ func main() {
 		"ImageBuilderConfig": cfg.ImageBuilderConfig.URL,
 	}).Info("Configuration Values:")
 	db.InitDB()
-	err := db.DB.AutoMigrate(&models.Commit{}, &models.UpdateTransaction{}, &models.Package{}, &models.Image{}, &models.Repo{}, &models.DispatchRecord{})
+	err := db.DB.AutoMigrate(&models.Commit{}, &models.UpdateTransaction{}, &models.Package{}, &models.Image{}, &models.Repo{}, &models.DispatchRecord{}, &models.ImageSet{})
 	if err != nil {
 		panic(err)
 	}
