d31d4cd4e7a237461f55f05c422721d3d3a3166a
Adelia Ferreira <tay.figueira@gmail.com>
2021-09-21 18:58:15 +0100

    Add validation in image creation to image name already in use and test cases for create and update image
    Signed-off-by: Adelia Ferreira <tay.figueira@gmail.com>

---
+++ pkg/models/main_test.go
@@ -0,0 +1,55 @@
+package models
+
+import (
+	"fmt"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/redhatinsights/edge-api/config"
+	"github.com/redhatinsights/edge-api/pkg/db"
+)
+
+func TestMain(m *testing.M) {
+	setUp()
+	retCode := m.Run()
+	tearDown()
+	os.Exit(retCode)
+}
+
+var dbName string
+
+func setUp() {
+	config.Init()
+	config.Get().Debug = true
+	time := time.Now().UnixNano()
+	dbName = fmt.Sprintf("%d-models.db", time)
+	config.Get().Database.Name = dbName
+	db.InitDB()
+	err := db.DB.AutoMigrate(
+		ImageSet{},
+		Commit{},
+		UpdateTransaction{},
+		Package{},
+		Image{},
+		Repo{},
+		Device{},
+		DispatchRecord{},
+	)
+	var testImage = Image{
+		Account:      "0000000",
+		Status:       ImageStatusBuilding,
+		Distribution: "rhel-8",
+		Name:         "image_name_pre_exist",
+		Commit:       &Commit{Arch: "x86_64"},
+		OutputTypes:  []string{ImageTypeCommit},
+	}
+	db.DB.Create(&testImage)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func tearDown() {
+	os.Remove(dbName)
+}
