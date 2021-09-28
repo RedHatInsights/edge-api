07e26a44f55b5cbfef71c65fb9522f26ec59c2e2
acosferreia <acosferreira@gmail.com>
2021-09-28 14:38:34 -0700

    fix join relationship and where clause to get correct relation; add test

--- pkg/services/updates_test.go
+++ pkg/services/updates_test.go
@@ -12,6 +12,7 @@ import (
 func TestGetUpdateTransactionsForDevice(t *testing.T) {
 
 	uuid := faker.UUIDHyphenated()
+	uu2d := faker.UUIDHyphenated()
 	updateService := UpdateService{
 		ctx: context.Background(),
 	}
@@ -20,6 +21,10 @@ func TestGetUpdateTransactionsForDevice(t *testing.T) {
 		UUID: uuid,
 	}
 	db.DB.Create(&device)
+	device2 := models.Device{
+		UUID: uu2d,
+	}
+	db.DB.Create(&device2)
 	updates := []models.UpdateTransaction{
 		{
 			Devices: []models.Device{
@@ -31,9 +36,15 @@ func TestGetUpdateTransactionsForDevice(t *testing.T) {
 				device,
 			},
 		},
+		{
+			Devices: []models.Device{
+				device2,
+			},
+		},
 	}
 	db.DB.Create(&updates[0])
 	db.DB.Create(&updates[1])
+	db.DB.Create(&updates[2])
 	actual, err := updateService.GetUpdateTransactionsForDevice(&device)
 	if actual == nil {
 		t.Errorf("Expected not nil updates")
@@ -43,5 +54,13 @@ func TestGetUpdateTransactionsForDevice(t *testing.T) {
 	if err != nil {
 		t.Errorf("Error not expected, got %s", err.Error())
 	}
-
+	updDevice2, err := updateService.GetUpdateTransactionsForDevice(&device2)
+	if updDevice2 == nil {
+		t.Errorf("Expected not nil updates")
+	} else if len(*updDevice2) != 1 {
+		t.Errorf("Expected one update transactions, got %d", len(*updDevice2))
+	}
+	if err != nil {
+		t.Errorf("Error not expected, got %s", err.Error())
+	}
 }
