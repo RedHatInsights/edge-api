305b705c274e648c0a4636dc4132b0ee17e52a3d
csantiago <resolutecoder@gmail.com>
2021-10-04 13:57:39 -0500

    created ddf form data and settings endpoint

--- pkg/routes/settings.go
+++ pkg/routes/settings.go
@@ -2,30 +2,47 @@ package routes
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/go-chi/chi"
+	"io/ioutil"
 	"net/http"
+	"os"
 )
 
 func MakeSettingsRouter(sub chi.Router) {
 	sub.Get("/", EdgeSettings)
 }
 
+type Forms []struct {
+	Fields []struct {
+		Component    string `json:"component"`
+		Label        string `json:"label"`
+		Name         string `json:"name"`
+		IsRequired   bool   `json:"isRequired,omitempty"`
+		Title        string `json:"title"`
+		HelpText     string `json:"helpText"`
+		InitialValue string `json:"initialValue"`
+		Description  string `json:"description"`
+		Validate     []struct {
+			Type string `json:"type"`
+		} `json:"validate,omitempty"`
+	} `json:"fields"`
+}
+
 func EdgeSettings(w http.ResponseWriter, r *http.Request) {
-	type Form []struct {
-		Fields []struct {
-			Component string `json:"component"`
-			Label     string `json:"label"`
-			Name      string `json:"name"`
-		} `json:"fields"`
+	jsonFile, err := os.Open("./pkg/services/template_form/form.json")
+	if err != nil {
+		fmt.Println(err)
 	}
+	fmt.Println("Successfully Opened users.json")
+	defer jsonFile.Close()
 
-	//	component := `
-	//	{
-	//		"component": "checkbox",
-	//		"label": "Checkbox",
-	//		"name": "checkbox"
-	//		}
-	//		`
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	
+	var forms Forms
+	json.Unmarshal(byteValue, &forms)
 
-	json.NewEncoder(w).Encode()
+	fmt.Println(forms)
+
+	json.NewEncoder(w).Encode(forms)
 }
