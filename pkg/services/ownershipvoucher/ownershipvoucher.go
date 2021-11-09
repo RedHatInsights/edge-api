238fdb32a12f4e76162f162a39ac57a524f40947
Aviel Yosef <ayosef@redhat.com>
2021-11-09 13:30:00 +0200

    Use libfdo_data
    libfdo_data will be shipped with RHEL, therefore no tests

---
+++ pkg/services/ownershipvoucher/ownershipvoucher.go
@@ -0,0 +1,40 @@
+package ownershipvoucher
+
+// libfdo_data will be shipped with RHEL
+// #cgo LDFLAGS: -lfdo_data
+// #include "fdo_data.h"
+import "C"
+import "errors"
+
+// Data requierd for a voucher
+type Data struct {
+	ProtocolVersion uint   `json:"protocol_version"`
+	GUID            string `json:"guid"`
+	DeviceName      string `json:"device_name"`
+}
+
+// ParseVoucher parses a voucher from a byte array, returning the data and error if any
+func ParseVoucher(voucherBytes []byte) (Data, error) {
+	voucherBytesLen := C.size_t(len(voucherBytes))
+	voucherCBytes := C.CBytes(voucherBytes)
+
+	voucher := C.fdo_ownershipvoucher_from_data(voucherCBytes, voucherBytesLen)
+	defer C.fdo_ownershipvoucher_free(voucher)
+	if voucher == nil {
+		return Data{}, errors.New("Failed to parse voucher")
+	}
+
+	guidC := C.fdo_ownershipvoucher_header_get_guid(voucher)
+	defer C.fdo_free_string(guidC)
+	guid := C.GoString(guidC)
+
+	devinfoC := C.fdo_ownershipvoucher_header_get_device_info_string(voucher)
+	defer C.fdo_free_string(devinfoC)
+	devinfo := C.GoString(devinfoC)
+
+	return Data{
+		ProtocolVersion: uint(C.fdo_ownershipvoucher_header_get_protocol_version(voucher)),
+		GUID:            guid,
+		DeviceName:      devinfo,
+	}, nil
+}
