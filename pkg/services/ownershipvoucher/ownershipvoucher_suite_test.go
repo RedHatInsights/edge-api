b695b84a14af565516aca543a856a0bf4d3349ff
Aviel Yosef <ayosef@redhat.com>
2021-11-09 14:21:27 +0200

    Adding tests

---
+++ pkg/services/ownershipvoucher/ownershipvoucher_suite_test.go
@@ -0,0 +1,13 @@
+package ownershipvoucher_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestOwnershipvoucher(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Ownershipvoucher Suite")
+}
