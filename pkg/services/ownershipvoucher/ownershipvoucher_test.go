2e3d290b7a9017ef5e85ab271d96f531860f7f6e
Aviel Yosef <ayosef@redhat.com>
2021-11-10 12:12:27 +0200

    Test bad ov

--- pkg/services/ownershipvoucher/ownershipvoucher_test.go
+++ pkg/services/ownershipvoucher/ownershipvoucher_test.go
@@ -17,12 +17,19 @@ var _ = Describe("Ownershipvoucher", func() {
 		})
 	})
 	Context("parse ov", func() {
-		data, err := ov.ParseVoucher(ovb)
 		It("should parse without error", func() {
+			data, err := ov.ParseVoucher(ovb)
 			Expect(err).To(BeNil())
 			Expect(data.ProtocolVersion).To(Equal(uint(100)))
 			Expect(data.DeviceName).To(Equal("testdevice1"))
 			Expect(data.GUID).To(Equal("214d64be-3227-92da-0333-b1e1fe832f24"))
+		})
+		It("should parse with error", func() {
+			badOV := ovb[1:]
+			data, err := ov.ParseVoucher(badOV)
+			Expect(err).ToNot(BeNil())
+			Expect(err.Error()).To(Equal("Failed to parse voucher"))
+			Expect(data).To(Equal(ov.Data{}))
 		})
 	})
 })
