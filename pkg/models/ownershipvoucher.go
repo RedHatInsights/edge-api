a0885c2bebf9d21daae0051ee641edfd599aad58
Aviel Yosef <ayosef@redhat.com>
2021-11-16 18:38:36 +0200

    Remove foreignKey

--- pkg/models/ownershipvoucher.go
+++ pkg/models/ownershipvoucher.go
@@ -2,6 +2,7 @@ package models
 
 // OwnershipVoucherData represents the data of an ownership voucher
 type OwnershipVoucherData struct {
+	Model
 	ProtocolVersion uint   `json:"protocol_version"`
 	GUID            string `json:"guid"`
 	DeviceName      string `json:"device_name"`
