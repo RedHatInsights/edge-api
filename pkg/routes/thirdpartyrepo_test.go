42e35a9d3570e74344e610c8220603e00b010d99
saurabh-thakre <thakre.sau@gmail.com>
2021-11-17 12:38:03 +0530

    adding tests

--- pkg/routes/thirdpartyrepo_test.go
+++ pkg/routes/thirdpartyrepo_test.go
@@ -3,6 +3,8 @@ package routes
 import (
 	"bytes"
 	"context"
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -80,5 +82,57 @@ func TestGetAllThirdPartyRepo(t *testing.T) {
 		t.Errorf("handler returned wrong status code: got %v, want %v",
 			status, http.StatusOK)
 
+	}
+}
+
+func TestGetAllThirdPartyRepoFilterParams(t *testing.T) {
+	tt := []struct {
+		name          string
+		params        string
+		expectedError []validationError
+	}{
+		{
+			name:   "bad created_at date",
+			params: "created_at=today",
+			expectedError: []validationError{
+				{Key: "created_at", Reason: `parsing time "today" as "2006-01-02": cannot parse "today" as "2006"`},
+			},
+		},
+		{
+			name:   "bad sort_by",
+			params: "sort_by=test",
+			expectedError: []validationError{
+				{Key: "sort_by", Reason: "test is not a valid sort_by. Sort-by must be name or created_at"},
+			},
+		},
+	}
+
+	next := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})
+	for _, te := range tt {
+		req, err := http.NewRequest("GET", fmt.Sprintf("/thirdpartyrepo?%s", te.params), nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		w := httptest.NewRecorder()
+		validateGetAllThirdPartyRepoFilterParams(next).ServeHTTP(w, req)
+
+		resp := w.Result()
+		jsonBody := []validationError{}
+		err = json.NewDecoder(resp.Body).Decode(&jsonBody)
+		if err != nil {
+			t.Errorf("failed decoding response body: %s", err.Error())
+		}
+		for _, exErr := range te.expectedError {
+			found := false
+			for _, jsErr := range jsonBody {
+				if jsErr.Key == exErr.Key && jsErr.Reason == exErr.Reason {
+					found = true
+					break
+				}
+			}
+			if !found {
+				t.Errorf("in %q: was expected to have %v but not found in %v", te.name, exErr, jsonBody)
+			}
+		}
 	}
 }
