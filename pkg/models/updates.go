9d13ed766e144e151f5783769b28039d87a20db4
Aviel Yosef <ayosef@redhat.com>
2021-11-17 09:30:25 +0200

    Add OwnershipVoucherDataID

--- pkg/models/updates.go
+++ pkg/models/updates.go
@@ -11,11 +11,12 @@ import (
 //	and 1 is reachable.
 type Device struct {
 	Model
-	UUID                 string                `json:"UUID"`
-	DesiredHash          string                `json:"DesiredHash"`
-	RHCClientID          string                `json:"RHCClientID"`
-	Connected            bool                  `gorm:"default:true" json:"Connected"`
-	OwnershipVoucherData *OwnershipVoucherData `json:"fdo"`
+	UUID                   string                `json:"UUID"`
+	DesiredHash            string                `json:"DesiredHash"`
+	RHCClientID            string                `json:"RHCClientID"`
+	Connected              bool                  `gorm:"default:true" json:"Connected"`
+	OwnershipVoucherDataID int                   `json:"-"`
+	OwnershipVoucherData   *OwnershipVoucherData `json:"fdo"`
 }
 
 // UpdateTransaction represents the combination of an OSTree commit and a set of Inventory
