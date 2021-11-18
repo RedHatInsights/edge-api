63a2e26f3b9cd4daaaa9d3d410c5e97cd49b87ae
Aviel Yosef <ayosef@redhat.com>
2021-11-18 15:53:00 +0200

    Add header valisation

--- pkg/routes/ownershipvoucher.go
+++ pkg/routes/ownershipvoucher.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/redhatinsights/edge-api/pkg/dependencies"
 	"github.com/redhatinsights/edge-api/pkg/errors"
+	"github.com/redhatinsights/edge-api/pkg/services"
 
 	"github.com/go-chi/chi"
 )
@@ -25,6 +26,14 @@ func MakeFDORouter(sub chi.Router) {
 func CreateOwnershipVouchers(w http.ResponseWriter, r *http.Request) {
 	services, _ := r.Context().Value(dependencies.Key).(*dependencies.EdgeAPIServices)
 	defer r.Body.Close()
+
+	validationErr := validateUploadRequestHeaders(r)
+	if validationErr != nil {
+		services.Log.Error("Couldn't validate ownership voucher upload request headers ", validationErr.Error())
+		w.WriteHeader(validationErr.GetStatus())
+		json.NewEncoder(w).Encode(validationErr)
+		return
+	}
 
 	decoder := cbor.NewDecoder(r.Body)
 	data := []byte{}
@@ -55,6 +64,14 @@ func DeleteOwnershipVouchers(w http.ResponseWriter, r *http.Request) {
 	services, _ := r.Context().Value(dependencies.Key).(*dependencies.EdgeAPIServices)
 	defer r.Body.Close()
 
+	validationErr := validateDeleteRequestHeaders(r)
+	if validationErr != nil {
+		services.Log.Error("Couldn't validate ownership voucher delete request headers ", validationErr.Error())
+		w.WriteHeader(validationErr.GetStatus())
+		json.NewEncoder(w).Encode(validationErr)
+		return
+	}
+
 	decoder := json.NewDecoder(r.Body)
 	data := []string{}
 	err := decoder.Decode(&data)
@@ -74,4 +91,29 @@ func DeleteOwnershipVouchers(w http.ResponseWriter, r *http.Request) {
 	}
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(resp)
+}
+
+// validate upload request headers
+func validateUploadRequestHeaders(r *http.Request) errors.APIError {
+	if r.Header.Get("Content-Type") != "application/cbor" {
+		return errors.NewBadRequest("Content-Type header must be application/cbor")
+	}
+	if r.Header.Get("Accept") != "application/json" {
+		return errors.NewBadRequest("Accept header must be set")
+	}
+	if vNum, err := strconv.Atoi(r.Header.Get("X-Number-Of-Vouchers")); vNum < 0 && err != nil {
+		return errors.NewBadRequest("X-Number-Of-Vouchers header must be set & greater than zero")
+	}
+	return nil
+}
+
+// validate delete request headers
+func validateDeleteRequestHeaders(r *http.Request) errors.APIError {
+	if r.Header.Get("Content-Type") != "application/json" {
+		return errors.NewBadRequest("Content-Type header must be application/json")
+	}
+	if r.Header.Get("Accept") != "application/json" {
+		return errors.NewBadRequest("Accept header must be set")
+	}
+	return nil
 }
