3599f4e85596d5e888ab11a0246179a0e5553872
Aviel Yosef <ayosef@redhat.com>
2021-11-18 15:57:09 +0200

    Add simple test

--- pkg/routes/ownershipvoucher_test.go
+++ pkg/routes/ownershipvoucher_test.go
@@ -1,19 +1,49 @@
 package routes_test
 
 import (
-	"github.com/golang/mock/gomock"
+	"bytes"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+
+	"github.com/go-chi/chi"
 	. "github.com/onsi/ginkgo"
-	// . "github.com/onsi/gomega"
-	// "github.com/redhatinsights/edge-api/pkg/routes"
-	// "github.com/redhatinsights/edge-api/pkg/services/mock_services"
+	. "github.com/onsi/gomega"
+	"github.com/redhatinsights/edge-api/pkg/routes"
 )
 
 var _ = Describe("Ownershipvoucher", func() {
-	var ctrl *gomock.Controller
-	BeforeEach(func() {
-		ctrl = gomock.NewController(GinkgoT())
+	ovb, err := ioutil.ReadFile("/testdevice1.ov")
+	m := chi.NewRouter()
+	m.Route("/", func(s chi.Router) {
+		routes.MakeFDORouter(s)
 	})
-	AfterEach(func() {
-		ctrl.Finish()
+
+	Context("read ov", func() {
+		It("should succeed", func() {
+			Expect(err).To(BeNil())
+			Expect(ovb).ToNot(BeNil())
+		})
+	})
+
+	Context("router validation", func() {
+		It("has two routes", func() {
+			Expect(m.Routes()).To(HaveLen(2))
+		})
+	})
+
+	Context("create ownership vouchers", func() {
+		req, err := http.NewRequest(http.MethodPost, "/ownership_voucher", bytes.NewBuffer(ovb))
+		It("create request without error", func() {
+			Expect(err).To(BeNil())
+		})
+		resRec := httptest.NewRecorder()
+		It("should succeed", func() {
+			req.Header.Add("Content-Type", "application/cbor")
+			req.Header.Add("X-Number-Of-Vouchers", "1")
+			req.Header.Add("Accept", "application/json")
+			m.ServeHTTP(resRec, req)
+			Expect(resRec.Code).To(Equal(http.StatusCreated))
+		})
 	})
 })
