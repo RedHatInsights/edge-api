33e5b47688031661f1a42a18b12c196ea786f856
Aviel Yosef <ayosef@redhat.com>
2021-12-02 19:19:51 +0200

    image_build_isourl -> image_build_iso_url
    not the perfect script :D

--- pkg/models/installers.go
+++ pkg/models/installers.go
@@ -4,7 +4,7 @@ package models
 type Installer struct {
 	Model
 	Account          string `json:"account"`
-	ImageBuildISOURL string `json:"image_build_isourl"`
+	ImageBuildISOURL string `json:"image_build_iso_url"`
 	ComposeJobID     string `json:"compose_job_id"`
 	Status           string `json:"status"`
 	Username         string `json:"username"`
