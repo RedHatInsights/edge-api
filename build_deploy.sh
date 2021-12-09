6a43c6968d3d3fef85a4ba940c8b83603138a79a
Thom Carlin <thom-at-redhat@users.noreply.github.com>
2021-12-09 16:02:28 -0500

    Added comments per PR

--- build_deploy.sh
+++ build_deploy.sh
@@ -3,6 +3,8 @@
 set -exv
 
 IMAGE="quay.io/cloudservices/edge-api"
+
+# Determine Git commit hash (7 hex characters)
 IMAGE_TAG="$(git rev-parse --short=7 HEAD)"
 
 if [[ -z "${QUAY_USER}" || -z "${QUAY_TOKEN}" ]]; then
@@ -21,14 +23,21 @@ export REGISTRY_AUTH_FILE="${AUTH_CONF_DIR}/auth.json"
 
 podman login -u="${QUAY_USER}" -p="${QUAY_TOKEN}" quay.io
 podman login -u="${RH_REGISTRY_USER}" -p="${RH_REGISTRY_TOKEN}" registry.redhat.io
+
+# Build image
 podman build -f Dockerfile -t "${IMAGE}:${IMAGE_TAG}" .
+
+# Push image to remote repository
 podman push "${IMAGE}:${IMAGE_TAG}"
 
-podman tag "${IMAGE}:${IMAGE_TAG}" "${IMAGE}:qa"
-podman push "${IMAGE}:qa"
-
+# Update LATEST tag on remote repository to this hash
 podman tag "${IMAGE}:${IMAGE_TAG}" "${IMAGE}:latest"
 podman push "${IMAGE}:latest"
 
+# Update MAIN tag on remote repository to this hash
 podman tag "${IMAGE}:${IMAGE_TAG}" "${IMAGE}:main"
 podman push "${IMAGE}:main"
+
+# Update QA tag on remote repository to this hash
+podman tag "${IMAGE}:${IMAGE_TAG}" "${IMAGE}:qa"
+podman push "${IMAGE}:qa"
