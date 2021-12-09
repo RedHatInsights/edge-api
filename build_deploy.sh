3c8d35c0949aef7401e359b2891c707673198bff
Thom Carlin <thom-at-redhat@users.noreply.github.com>
2021-12-09 15:57:31 -0500

    THEEDGE-1426: Added "latest" and "main" tags, corrected some shellcheck issues

--- build_deploy.sh
+++ build_deploy.sh
@@ -3,25 +3,32 @@
 set -exv
 
 IMAGE="quay.io/cloudservices/edge-api"
-IMAGE_TAG=$(git rev-parse --short=7 HEAD)
+IMAGE_TAG="$(git rev-parse --short=7 HEAD)"
 
-if [[ -z "$QUAY_USER" || -z "$QUAY_TOKEN" ]]; then
+if [[ -z "${QUAY_USER}" || -z "${QUAY_TOKEN}" ]]; then
     echo "QUAY_USER and QUAY_TOKEN must be set"
     exit 1
 fi
 
-if [[ -z "$RH_REGISTRY_USER" || -z "$RH_REGISTRY_TOKEN" ]]; then
+if [[ -z "${RH_REGISTRY_USER}" || -z "${RH_REGISTRY_TOKEN}" ]]; then
     echo "RH_REGISTRY_USER and RH_REGISTRY_TOKEN  must be set"
     exit 1
 fi
 
 AUTH_CONF_DIR="$(pwd)/.podman"
-mkdir -p $AUTH_CONF_DIR
-export REGISTRY_AUTH_FILE="$AUTH_CONF_DIR/auth.json"
+mkdir -p "${AUTH_CONF_DIR}"
+export REGISTRY_AUTH_FILE="${AUTH_CONF_DIR}/auth.json"
 
-podman login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
-podman login -u="$RH_REGISTRY_USER" -p="$RH_REGISTRY_TOKEN" registry.redhat.io
+podman login -u="${QUAY_USER}" -p="${QUAY_TOKEN}" quay.io
+podman login -u="${RH_REGISTRY_USER}" -p="${RH_REGISTRY_TOKEN}" registry.redhat.io
 podman build -f Dockerfile -t "${IMAGE}:${IMAGE_TAG}" .
 podman push "${IMAGE}:${IMAGE_TAG}"
+
 podman tag "${IMAGE}:${IMAGE_TAG}" "${IMAGE}:qa"
 podman push "${IMAGE}:qa"
+
+podman tag "${IMAGE}:${IMAGE_TAG}" "${IMAGE}:latest"
+podman push "${IMAGE}:latest"
+
+podman tag "${IMAGE}:${IMAGE_TAG}" "${IMAGE}:main"
+podman push "${IMAGE}:main"
