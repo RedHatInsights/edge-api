ed23373a6d4951c4cc16b1c07b75a1e78e195023
acosferreia <acosferreira@gmail.com>
2021-12-10 17:15:37 -0800

    fix lint

--- pkg/services/updates.go
+++ pkg/services/updates.go
@@ -216,6 +216,7 @@ func (s *UpdateService) getPlaybookURL(updateID uint) string {
 	return url
 }
 
+// WriteTemplate is the function that writes template to be executed
 func (s *UpdateService) WriteTemplate(templateInfo TemplateRemoteInfo, account string) (string, error) {
 	cfg := config.Get()
 	filePath := cfg.TemplatesPath
