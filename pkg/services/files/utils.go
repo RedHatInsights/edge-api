c4f69bce4e1b3263c5d815226f9dd616b9465660
Bianca Rosa <me@biancarosa.com.br>
2022-01-27 17:09:49 -0300

    Update sanitize path method

--- pkg/services/files/utils.go
+++ pkg/services/files/utils.go
@@ -8,7 +8,7 @@ import (
 )
 
 func sanitizePath(destination string, filePath string) (destpath string, err error) {
-	destpath = filepath.Clean(filepath.Join(destination, filePath))
+	destpath = filepath.Join(destination, filePath)
 	prefix := filepath.Clean(destination + string(os.PathSeparator))
 	if !strings.HasPrefix(destpath, prefix) {
 		err = fmt.Errorf("%s: illegal file path, prefix: %s, destpath: %s", filePath, prefix, destpath)
