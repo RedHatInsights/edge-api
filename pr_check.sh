899f4cb1993587bda34da24df5aee560bf7dfbd6
James Wong <2421248+jameswnl@users.noreply.github.com>
2022-01-31 11:35:47 -0500

    Update pr_check.sh

--- pr_check.sh
+++ pr_check.sh
@@ -16,7 +16,7 @@ CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
 curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh
 echo $?
 ls -l .cicd_bootstrap.sh
-source .cicd_bootstrap.sh
+source ./.cicd_bootstrap.sh
 
 # Build the image and push to quay
 source $CICD_ROOT/build.sh
