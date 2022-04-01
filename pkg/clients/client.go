e683d1495a6d57c51ac89ef51e9899b808fcf6da
Demetrius Costa Silva Faria Lima <delima@redhat.com>
2022-04-01 12:46:53 -0300

    create common to manage the identity

--- pkg/clients/client.go
+++ pkg/clients/client.go
@@ -2,16 +2,12 @@ package clients
 
 import (
 	"context"
+	"github.com/redhatinsights/edge-api/pkg/routes/common"
 	"github.com/redhatinsights/platform-go-middlewares/request_id"
 	log "github.com/sirupsen/logrus"
 
 	"github.com/redhatinsights/edge-api/config"
 )
-
-type rhIdentityKeyType string
-
-// RhIdentityKey is the context key for x-rh-identity
-const RhIdentityKey = rhIdentityKeyType("xRhIdentity")
 
 // GetOutgoingHeaders returns Red Hat Insights headers from our request to use it
 // in other request that will be used when communicating in Red Hat Insights services
@@ -19,11 +15,12 @@ func GetOutgoingHeaders(ctx context.Context) map[string]string {
 	requestID := request_id.GetReqID(ctx)
 	headers := map[string]string{"x-rh-insights-request-id": requestID}
 	if config.Get().Auth {
-		xrhid := ctx.Value(RhIdentityKey)
-		if xrhid == nil {
+		xrhid, err := common.GetOriginalIdentity(ctx)
+
+		if err != nil {
 			log.Error("Error getting x-rh-identity")
 		} else {
-			headers["x-rh-identity"] = xrhid.(string)
+			headers["x-rh-identity"] = xrhid
 		}
 	}
 	return headers
