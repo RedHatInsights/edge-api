6eab9282f4b407c11a308f8626789b34c7e1a017
Thom Carlin <thom-at-redhat@users.noreply.github.com>
2022-04-07 07:35:18 -0400

    Change migrate/wipe to better handle errors and panics

--- cmd/db/wipe.go
+++ cmd/db/wipe.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	"github.com/redhatinsights/edge-api/config"
 	l "github.com/redhatinsights/edge-api/logger"
 	"github.com/redhatinsights/edge-api/pkg/db"
@@ -9,6 +11,12 @@ import (
 	"gorm.io/gorm"
 )
 
+func handlePanic(errorOccurred *bool) {
+	if err := recover(); err != nil {
+		log.Error("Database deletion failure")
+		os.Exit(1)
+	}
+}
 func main() {
 	config.Init()
 	l.InitLogger()
@@ -35,31 +43,97 @@ func main() {
 	db.InitDB()
 
 	log.Info("Cleaning up relations and old tables (which may not exist)...")
-	db.DB.Session(&gorm.Session{AllowGlobalUpdate: true}).Unscoped().Exec("DELETE FROM commit_packages")
-	db.DB.Session(&gorm.Session{AllowGlobalUpdate: true}).Unscoped().Exec("DELETE FROM commit_installed_packages")
-	db.DB.Session(&gorm.Session{AllowGlobalUpdate: true}).Unscoped().Exec("DELETE FROM device_groups_devices")
-	db.DB.Session(&gorm.Session{AllowGlobalUpdate: true}).Unscoped().Exec("DELETE FROM images_packages")
-	db.DB.Session(&gorm.Session{AllowGlobalUpdate: true}).Unscoped().Exec("DELETE FROM updatetransaction_devices")
-	db.DB.Session(&gorm.Session{AllowGlobalUpdate: true}).Unscoped().Exec("DROP TABLE updaterecord_commits")
-	db.DB.Session(&gorm.Session{AllowGlobalUpdate: true}).Unscoped().Exec("DROP TABLE updaterecord_devices")
-	db.DB.Session(&gorm.Session{AllowGlobalUpdate: true}).Unscoped().Exec("DROP TABLE update_records")
-	db.DB.Session(&gorm.Session{AllowGlobalUpdate: true}).Unscoped().Exec("ALTER TABLE commits DROP CONSTRAINT fk_commits_repo")
+	errorOccurred := false
+	defer handlePanic(&errorOccurred)
+
+	var sqlStatements = make([]string, 0)
+	sqlStatements = append(sqlStatements, "DELETE FROM commit_packages")
+
+	sqlStatements = append(sqlStatements, "DELETE FROM commit_installed_packages")
+
+	sqlStatements = append(sqlStatements, "DELETE FROM device_groups_devices")
+
+	sqlStatements = append(sqlStatements, "DELETE FROM images_packages")
+
+	sqlStatements = append(sqlStatements, "DELETE FROM updatetransaction_devices")
+
+	sqlStatements = append(sqlStatements, "DROP TABLE updaterecord_commits")
+
+	sqlStatements = append(sqlStatements, "DROP TABLE updaterecord_devices")
+
+	sqlStatements = append(sqlStatements, "DROP TABLE update_records")
+
+	sqlStatements = append(sqlStatements, "ALTER TABLE commits DROP CONSTRAINT fk_commits_repo")
+
+	for sqlIndex, sqlStatement := range sqlStatements {
+		log.Debugf("Running SQL statement %d: %s", sqlIndex, sqlStatement)
+		sqlResult := db.DB.Session(&gorm.Session{AllowGlobalUpdate: true}).Unscoped().Exec(sqlStatement)
+		if sqlResult.Error != nil {
+			log.Warningf("SQL statement failure %s", sqlResult.Error)
+			errorOccurred = true
+		}
+	}
 
 	log.Info("Starting deleting of models...")
-	// Order should match Automigration cmd/migrate/main.go
-	db.DB.Session(&gorm.Session{AllowGlobalUpdate: true}).Unscoped().Delete(&models.Commit{})
-	db.DB.Session(&gorm.Session{AllowGlobalUpdate: true}).Unscoped().Delete(&models.DeviceGroup{})
-	db.DB.Session(&gorm.Session{AllowGlobalUpdate: true}).Unscoped().Delete(&models.DispatchRecord{})
-	db.DB.Session(&gorm.Session{AllowGlobalUpdate: true}).Unscoped().Delete(&models.FDODevice{})
-	db.DB.Session(&gorm.Session{AllowGlobalUpdate: true}).Unscoped().Delete(&models.FDOUser{})
-	db.DB.Session(&gorm.Session{AllowGlobalUpdate: true}).Unscoped().Delete(&models.Image{})
-	db.DB.Session(&gorm.Session{AllowGlobalUpdate: true}).Unscoped().Delete(&models.ImageSet{})
-	db.DB.Session(&gorm.Session{AllowGlobalUpdate: true}).Unscoped().Delete(&models.Installer{})
-	db.DB.Session(&gorm.Session{AllowGlobalUpdate: true}).Unscoped().Delete(&models.OwnershipVoucherData{})
-	db.DB.Session(&gorm.Session{AllowGlobalUpdate: true}).Unscoped().Delete(&models.Package{})
-	db.DB.Session(&gorm.Session{AllowGlobalUpdate: true}).Unscoped().Delete(&models.Repo{})
-	db.DB.Session(&gorm.Session{AllowGlobalUpdate: true}).Unscoped().Delete(&models.SSHKey{})
-	db.DB.Session(&gorm.Session{AllowGlobalUpdate: true}).Unscoped().Delete(&models.ThirdPartyRepo{})
-	db.DB.Session(&gorm.Session{AllowGlobalUpdate: true}).Unscoped().Delete(&models.UpdateTransaction{})
-	log.Info("Wipe Completed")
+	// Order is empircally determine and should be inverse of Automigration cmd/migrate/main.go
+	var modelsInterfaces = make([]interface{}, 0)
+	var modelsLabels = make([]string, 0)
+
+	modelsInterfaces = append(modelsInterfaces, &models.DeviceGroup{})
+	modelsLabels = append(modelsLabels, "DeviceGroup")
+
+	modelsInterfaces = append(modelsInterfaces, &models.DispatchRecord{})
+	modelsLabels = append(modelsLabels, "DispatchRecord")
+
+	modelsInterfaces = append(modelsInterfaces, &models.FDODevice{})
+	modelsLabels = append(modelsLabels, "FDODevice")
+
+	modelsInterfaces = append(modelsInterfaces, &models.FDOUser{})
+	modelsLabels = append(modelsLabels, "FDOUser")
+
+	modelsInterfaces = append(modelsInterfaces, &models.ImageSet{})
+	modelsLabels = append(modelsLabels, "ImageSet")
+
+	modelsInterfaces = append(modelsInterfaces, &models.Image{})
+	modelsLabels = append(modelsLabels, "Image")
+
+	modelsInterfaces = append(modelsInterfaces, &models.Commit{})
+	modelsLabels = append(modelsLabels, "Commit")
+
+	modelsInterfaces = append(modelsInterfaces, &models.Installer{})
+	modelsLabels = append(modelsLabels, "Installer")
+
+	modelsInterfaces = append(modelsInterfaces, &models.OwnershipVoucherData{})
+	modelsLabels = append(modelsLabels, "OwnershipVoucherData")
+
+	modelsInterfaces = append(modelsInterfaces, &models.Package{})
+	modelsLabels = append(modelsLabels, "Package")
+
+	modelsInterfaces = append(modelsInterfaces, &models.Repo{})
+	modelsLabels = append(modelsLabels, "Repo")
+
+	modelsInterfaces = append(modelsInterfaces, &models.SSHKey{})
+	modelsLabels = append(modelsLabels, "SSHKey")
+
+	modelsInterfaces = append(modelsInterfaces, &models.ThirdPartyRepo{})
+	modelsLabels = append(modelsLabels, "ThirdPartyRepo")
+
+	modelsInterfaces = append(modelsInterfaces, &models.UpdateTransaction{})
+	modelsLabels = append(modelsLabels, "UpdateTransaction")
+
+	for modelsIndex, modelsInterface := range modelsInterfaces {
+		log.Debugf("Removing Model %s", modelsLabels[modelsIndex])
+		result := db.DB.Session(&gorm.Session{AllowGlobalUpdate: true}).Unscoped().Delete(modelsInterface)
+		if result.Error != nil {
+			log.Warningf("database delete failure %s", result.Error)
+			errorOccurred = true
+		}
+	}
+
+	if !errorOccurred {
+		log.Info("Database wipe completed")
+	} else {
+		log.Error("Database wipe completed with errors")
+		os.Exit(2)
+	}
 }
--- cmd/migrate/main.go
+++ cmd/migrate/main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"os"
+
 	"github.com/redhatinsights/edge-api/config"
 	l "github.com/redhatinsights/edge-api/logger"
 	"github.com/redhatinsights/edge-api/pkg/db"
 	"github.com/redhatinsights/edge-api/pkg/models"
 	log "github.com/sirupsen/logrus"
 )
+
+func handlePanic(errorOccurred *bool) {
+	if err := recover(); err != nil {
+		log.Error("Database automigrate failure")
+		os.Exit(1)
+	}
+}
 
 func main() {
 	config.Init()
@@ -71,25 +80,71 @@ func main() {
 		}
 	*/
 	// Automigration
-	// Order should match Deleting of models in cmd/db/wipe.go
+	errorOccurred := false
+	defer handlePanic(&errorOccurred)
+
+	// Order should match model deletions in cmd/db/wipe.go
 	// Order is not strictly alphabetical due to dependencies (e.g. Image needs ImageSet)
-	err := db.DB.Debug().AutoMigrate(&models.Commit{},
-		&models.DeviceGroup{},
-		&models.DispatchRecord{},
-		&models.FDODevice{},
-		&models.FDOUser{},
-		&models.ImageSet{},
-		&models.Image{},
-		&models.Installer{},
-		&models.OwnershipVoucherData{},
-		&models.Package{},
-		&models.Repo{},
-		&models.SSHKey{},
-		&models.ThirdPartyRepo{},
-		&models.UpdateTransaction{})
-	if err != nil {
-		l.LogErrorAndPanic("database automigrate failure", err)
+	var modelsInterfaces = make([]interface{}, 0)
+	var modelsLabels = make([]string, 0)
+
+	modelsInterfaces = append(modelsInterfaces, &models.Commit{})
+	modelsLabels = append(modelsLabels, "Commit")
+
+	modelsInterfaces = append(modelsInterfaces, &models.DeviceGroup{})
+	modelsLabels = append(modelsLabels, "DeviceGroup")
+
+	modelsInterfaces = append(modelsInterfaces, &models.DispatchRecord{})
+	modelsLabels = append(modelsLabels, "DispatchRecord")
+
+	modelsInterfaces = append(modelsInterfaces, &models.FDODevice{})
+	modelsLabels = append(modelsLabels, "FDODevice")
+
+	modelsInterfaces = append(modelsInterfaces, &models.FDOUser{})
+	modelsLabels = append(modelsLabels, "FDOUser")
+
+	modelsInterfaces = append(modelsInterfaces, &models.ImageSet{})
+	modelsLabels = append(modelsLabels, "ImageSet")
+
+	modelsInterfaces = append(modelsInterfaces, &models.Image{})
+	modelsLabels = append(modelsLabels, "Image")
+
+	modelsInterfaces = append(modelsInterfaces, &models.Installer{})
+	modelsLabels = append(modelsLabels, "Installer")
+
+	modelsInterfaces = append(modelsInterfaces, &models.OwnershipVoucherData{})
+	modelsLabels = append(modelsLabels, "OwnershipVoucherData")
+
+	modelsInterfaces = append(modelsInterfaces, &models.Package{})
+	modelsLabels = append(modelsLabels, "Package")
+
+	modelsInterfaces = append(modelsInterfaces, &models.Repo{})
+	modelsLabels = append(modelsLabels, "Repo")
+
+	modelsInterfaces = append(modelsInterfaces, &models.SSHKey{})
+	modelsLabels = append(modelsLabels, "SSHKey")
+
+	modelsInterfaces = append(modelsInterfaces, &models.ThirdPartyRepo{})
+	modelsLabels = append(modelsLabels, "ThirdPartyRepo")
+
+	modelsInterfaces = append(modelsInterfaces, &models.UpdateTransaction{})
+	modelsLabels = append(modelsLabels, "UpdateTransaction")
+
+	for modelsIndex, modelsInterface := range modelsInterfaces {
+		log.Debugf("Migrating Model %s", modelsLabels[modelsIndex])
+
+		// err := db.DB.Debug().AutoMigrate( modelsInterface )
+		err := db.DB.AutoMigrate(modelsInterface)
+		if err != nil {
+			log.Warningf("database automigrate failure", err)
+			errorOccurred = true
+		}
 	}
 
-	log.Info("Migration Completed")
+	if !errorOccurred {
+		log.Info("Migration completed successfully")
+	} else {
+		log.Error("Migration completed with errors")
+		os.Exit(2)
+	}
 }
