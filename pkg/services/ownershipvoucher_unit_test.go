9ff9be0bb85fff188d42f8d70719d3f72aedcb6c
Demetrius Costa Silva Faria Lima <delima@redhat.com>
2022-05-19 18:15:02 -0300

    fix fdo device test

--- pkg/services/ownershipvoucher_unit_test.go
+++ pkg/services/ownershipvoucher_unit_test.go
@@ -123,7 +123,7 @@ var _ = Describe("Ownershipvoucher unit tests", func() {
 			err = device.BeforeDelete(db.DB)
 			Expect(err).To(BeNil())
 			// device should exist after `BeforeDelete`
-			device, err = ownershipVoucherService.GetFDODeviceByGUID(ov.GUID)
+			device, err = db.DB.Find(&models.FDODevice{}, "id = ?", device.id)
 			Expect(device).ToNot(BeNil())
 			Expect(err).To(BeNil())
 			Expect(device.DeletedAt.Valid).To(BeFalse())                     // not deleted
