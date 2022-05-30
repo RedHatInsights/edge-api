6f0ae624d7e321302ef2e19a4742470e9a0400b2
acosferreia <acosferreira@gmail.com>
2022-05-30 11:35:16 -0700

    fix lint

--- config/repo_config.go
+++ config/repo_config.go
@@ -1,14 +1,22 @@
 package config
 
+// RequiredPackages contains minimun list of packages to build an image
 var RequiredPackages = []string{"rhc",
 	"rhc-worker-playbook",
 	"subscription-manager",
 	"subscription-manager-plugin-ostree",
 	"insights-client"}
+
+// RHEL8 contains aditional list of packages to build an image to >= RHEL85
 var RHEL8 = []string{"ansible"}
+
+// RHEL86 contains aditional list of packages to build an image to = RHEL86
 var RHEL86 = []string{"ansible-core"}
+
+// RHEL90 contains aditional list of packages to build an image to = RHEL90
 var RHEL90 = []string{"ansible-core"}
 
+// DistributionsPackages add packages byi mage
 var DistributionsPackages = map[string][]string{
 	"rhel-84": RHEL8,
 	"rhel-85": RHEL8,
@@ -16,6 +24,7 @@ var DistributionsPackages = map[string][]string{
 	"rhel-90": RHEL90,
 }
 
+// DistributionsRefs set the ref to Images
 var DistributionsRefs = map[string]string{
 	"rhel-84": "rhel/8/x86_64/edge",
 	"rhel-85": "rhel/8/x86_64/edge",
