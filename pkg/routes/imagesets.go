5f37cb23a3ed0d8b38954cfe51ec3ebcd82253db
saurabh-thakre <thakre.sau@gmail.com>
2022-06-21 12:20:34 +0530

    removed status from the filter

--- pkg/routes/imagesets.go
+++ pkg/routes/imagesets.go
@@ -38,10 +38,10 @@ func MakeImageSetsRouter(sub chi.Router) {
 
 var imageSetFilters = common.ComposeFilters(
 
-	common.ContainFilterHandler(&common.Filter{
-		QueryParam: "status",
-		DBField:    "images.status",
-	}),
+	// common.ContainFilterHandler(&common.Filter{
+	// 	QueryParam: "status",
+	// 	DBField:    "images.status",
+	// }),
 	common.ContainFilterHandler(&common.Filter{
 		QueryParam: "name",
 		DBField:    "image_sets.name",
