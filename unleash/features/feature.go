73477a131e25702fe6b1f8bef0756af5015fc9c7
Aaron Hetherington <ahetheri@redhat.com>
2022-06-22 13:43:23 +0100

    Added feature flags to image apis.

--- unleash/features/feature.go
+++ unleash/features/feature.go
@@ -8,10 +8,19 @@ import (
 //FeatureCustomRepos is the const of the custom repo feature flag
 const FeatureCustomRepos = "fleet-management.custom-repos"
 
+//FeatureImageBuildMS is the const of the ms build feature flag
+const FeatureImageBuildMS = "fleet-management.images_iso"
+
 // CheckFeatureWithAccount checks to see if a given feature is available for a given account
 func CheckFeatureWithAccount(account string, feature string) bool {
 	unleashCtx := unleashCTX.Context{
 		UserId: account,
 	}
+	return unleash.IsEnabled(feature, unleash.WithContext(unleashCtx))
+}
+
+// CheckFeature checks to see if a given feature is available
+func CheckFeature(feature string) bool {
+	unleashCtx := unleashCTX.Context{}
 	return unleash.IsEnabled(feature, unleash.WithContext(unleashCtx))
 }
