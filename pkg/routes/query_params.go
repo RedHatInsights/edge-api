c995736e48d554b399497bc9a59c1989294a7823
ylahav <ylahav@redhat.com>
2022-07-13 16:45:39 +0300

    add sort_by query param for thirdpartyrepo endpoint

--- pkg/routes/query_params.go
+++ pkg/routes/query_params.go
@@ -20,7 +20,7 @@ func initalizeQueryParamsArray() map[string][]string {
 		m["devices"] = []string{"limit", "offset", "name", "uuid", "update_available", "image_id"}
 		m["images"] = []string{"limit", "offset", "status", "name", "distribution", "created_at"}
 		m["image-sets"] = []string{"limit", "offset", "status", "name", "version"}
-		m["thirdpartyrepo"] = []string{"limit", "offset", "name", "created_at", "updated_at"}
+		m["thirdpartyrepo"] = []string{"limit", "offset", "name", "created_at", "updated_at", "sort_by"}
 	}
 	return m
 }
