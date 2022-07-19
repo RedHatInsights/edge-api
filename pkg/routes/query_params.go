1923cbafbe1074590470a84f3d36952210102325
ylahav <ylahav@redhat.com>
2022-07-19 16:51:15 +0300

    add a fix to query-params

--- pkg/routes/query_params.go
+++ pkg/routes/query_params.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"net/http"
 	"reflect"
-	"strings"
 
 	"github.com/redhatinsights/edge-api/pkg/dependencies"
 )
@@ -17,7 +16,8 @@ func initalizeQueryParamsArray() map[string][]string {
 	if len(m) == 0 {
 		m = make(map[string][]string)
 		m["device-groups"] = []string{"limit", "offset", "name", "created_at", "updated_at", "sort_by"}
-		m["devices"] = []string{"limit", "offset", "name", "uuid", "update_available", "image_id", "sort_by"}
+		m["devices"] = []string{"per_page", "page", "sort_by", "order_how", "hostname_or_id"}
+		m["devicesview"] = []string{"limit", "offset", "name", "uuid", "update_available", "image_id", "sort_by"}
 		m["images"] = []string{"limit", "offset", "status", "name", "distribution", "created_at", "sort_by"}
 		m["image-sets"] = []string{"limit", "offset", "status", "name", "version", "sort_by"}
 		m["thirdpartyrepo"] = []string{"limit", "offset", "name", "created_at", "updated_at", "sort_by"}
@@ -34,30 +34,33 @@ func GetQueryParamsArray(endpoint string) []string {
 }
 
 // ValidateQueryParams validate the query params from the url are supported
-func ValidateQueryParams(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		var errs []validationError
-		path := strings.Split(r.URL.Path, "/")
-		endpoint := path[len(path)-1]
-		filtersMap := r.URL.Query()
-		queriesKeys := reflect.ValueOf(filtersMap).MapKeys()
-		qparamsArray := GetQueryParamsArray(endpoint)
-		// interating over the queries keys to validate we support those
-		for _, key := range queriesKeys {
-			if !(contains(qparamsArray, key.String())) {
-				qkey := key.String()
-				errs = append(errs, validationError{Key: qkey, Reason: fmt.Sprintf("%s is not a valid query param, supported query params: %s", qkey, qparamsArray)})
-			}
-		}
+func ValidateQueryParams(endpoint string) func(next http.Handler) http.Handler {
 
-		if len(errs) == 0 {
-			next.ServeHTTP(w, r)
-			return
-		}
-		w.WriteHeader(http.StatusBadRequest)
-		if err := json.NewEncoder(w).Encode(&errs); err != nil {
-			ctxServices := dependencies.ServicesFromContext(r.Context())
-			ctxServices.Log.WithField("error", errs).Error("Error while trying to encode device groups query-params validation errors")
-		}
-	})
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			var errs []validationError
+			// path := strings.Split(r.URL.Path, "/")
+			// endpoint := path[len(path)-1]
+			filtersMap := r.URL.Query()
+			queriesKeys := reflect.ValueOf(filtersMap).MapKeys()
+			qparamsArray := GetQueryParamsArray(endpoint)
+			// interating over the queries keys to validate we support those
+			for _, key := range queriesKeys {
+				if !(contains(qparamsArray, key.String())) {
+					qkey := key.String()
+					errs = append(errs, validationError{Key: qkey, Reason: fmt.Sprintf("%s is not a valid query param, supported query params: %s", qkey, qparamsArray)})
+				}
+			}
+
+			if len(errs) == 0 {
+				next.ServeHTTP(w, r)
+				return
+			}
+			w.WriteHeader(http.StatusBadRequest)
+			if err := json.NewEncoder(w).Encode(&errs); err != nil {
+				ctxServices := dependencies.ServicesFromContext(r.Context())
+				ctxServices.Log.WithField("error", errs).Error("Error while trying to encode device groups query-params validation errors")
+			}
+		})
+	}
 }
