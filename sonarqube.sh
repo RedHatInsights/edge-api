0afca917054abbcf9bfc2e7941425396340ea2a8
Thom Carlin <thom-at-redhat@users.noreply.github.com>
2022-08-16 10:48:33 -0400

    Run in java container like marketplace does

--- sonarqube.sh
+++ sonarqube.sh
@@ -19,13 +19,7 @@ curl -o "${PWD}/sonarqube/certs/RH-IT-Root-CA.crt" --insecure "${ROOT_CA_CERT_UR
   -noprompt
 
 export SONAR_SCANNER_OPTS="-Djavax.net.ssl.trustStore=${PWD}/sonarqube/store/RH-IT-Root-CA.keystore -Djavax.net.ssl.trustStorePassword=redhat"
-
-
 export SONAR_SCANNER_OS="linux"
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    export SONAR_SCANNER_OS="macosx"
-fi
-
 export SONAR_SCANNER_CLI_VERSION="4.7.0.2747"
 export SONAR_SCANNER_DOWNLOAD_NAME="sonar-scanner-cli-${SONAR_SCANNER_CLI_VERSION}-${SONAR_SCANNER_OS}"
 export SONAR_SCANNER_NAME="sonar-scanner-${SONAR_SCANNER_CLI_VERSION}-${SONAR_SCANNER_OS}"
@@ -38,12 +32,19 @@ export PATH="${PWD}/sonarqube/extract/${SONAR_SCANNER_NAME}/bin:${PATH}"
 
 COMMIT_SHORT=$(git rev-parse --short=7 HEAD)
 
-sonar-scanner \
-  -Dsonar.projectKey="console.redhat.com:fleet-management" \
-  -Dsonar.sources="./pkg" \
-  -Dsonar.host.url="${SONARQUBE_REPORT_URL}" \
-  -Dsonar.projectVersion="${COMMIT_SHORT}" \
-  -Dsonar.login="${SONARQUBE_TOKEN}"
+docker pull registry.access.redhat.com/openjdk/openjdk-11-rhel7:1.12-1.1658422675
+
+{ echo "SONARQUBE_REPORT_URL=${SONARQUBE_REPORT_URL}";
+  echo "COMMIT_SHORT=${COMMIT_SHORT}";
+  echo "SONARQUBE_TOKEN=${SONARQUBE_TOKEN}";
+  echo "SONAR_SCANNER_NAME=${SONAR_SCANNER_NAME}"
+} >> "${PWD}/sonarqube/my-env.txt"
+
+docker run \
+    -v"${PWD}":/home/jboss \
+    --env-file "${PWD}/sonarqube/my-env.txt" \
+    registry.access.redhat.com/openjdk/openjdk-11-rhel7:1.12-1.1658422675 \
+    "/home/jboss/sonarqube_exec.sh"
 
 mkdir -p "${WORKSPACE}/artifacts"
 cat << EOF > "${WORKSPACE}/artifacts/junit-dummy.xml"
