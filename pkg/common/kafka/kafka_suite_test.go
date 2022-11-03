a2a5bf2e2267632ec45e70311e79f8942a060c03
Thom Carlin <thom-at-redhat@users.noreply.github.com>
2022-11-03 08:02:02 -0400

    Fix issues reported by github actions golangci-lint #1

--- pkg/common/kafka/kafka_suite_test.go
+++ pkg/common/kafka/kafka_suite_test.go
@@ -8,6 +8,6 @@ import (
 )
 
 func TestKafka(t *testing.T) {
-	RegisterFailHandler(Fail) // nolint:typecheck
+	RegisterFailHandler(Fail) // nolint:gofmt,goimports,typecheck
 	RunSpecs(t, "Kafka Suite") // nolint:typecheck
 }
