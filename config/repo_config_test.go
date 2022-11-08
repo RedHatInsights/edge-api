3cdc937cff1b993b42ec8518327d3450e7cbc79c
acosferreia <acosferreira@gmail.com>
2022-11-08 14:19:27 -0800

    add test setup

---
+++ config/repo_config_test.go
@@ -0,0 +1,75 @@
+// FIXME: golangci-lint
+// nolint:errcheck,gosec,govet,revive,typecheck
+package config_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/redhatinsights/edge-api/config"
+)
+
+var _ = Describe("Validates repo config", func() {
+
+	distri8 := []string{"rhel-84", "rhel-85"}
+	distri8X := []string{"rhel-86", "rhel-87"}
+	distri9 := []string{"rhel-90"}
+
+	Context("Validate distribution ref values", func() {
+		It("should return RHEL8 distribution refs", func() {
+			for _, d := range distri8 {
+				Expect(config.DistributionsRefs[d]).ToNot(BeNil())
+				Expect(config.DistributionsRefs[d]).To(ContainSubstring("rhel"))
+				Expect(config.DistributionsRefs[d]).To(ContainSubstring("x86_64"))
+				Expect(config.DistributionsRefs[d]).To(ContainSubstring("edge"))
+				Expect(config.DistributionsRefs[d]).To(ContainSubstring("8"))
+
+			}
+		})
+		It("should return RHEL8X distribution refs", func() {
+			for _, d := range distri8X {
+				Expect(config.DistributionsRefs[d]).ToNot(BeNil())
+				Expect(config.DistributionsRefs[d]).To(ContainSubstring("rhel"))
+				Expect(config.DistributionsRefs[d]).To(ContainSubstring("x86_64"))
+				Expect(config.DistributionsRefs[d]).To(ContainSubstring("edge"))
+				Expect(config.DistributionsRefs[d]).To(ContainSubstring("8"))
+
+			}
+		})
+		It("should return RHEL9 distribution refs", func() {
+			for _, d := range distri9 {
+				Expect(config.DistributionsRefs[d]).ToNot(BeNil())
+				Expect(config.DistributionsRefs[d]).To(ContainSubstring("rhel"))
+				Expect(config.DistributionsRefs[d]).To(ContainSubstring("x86_64"))
+				Expect(config.DistributionsRefs[d]).To(ContainSubstring("edge"))
+				Expect(config.DistributionsRefs[d]).To(ContainSubstring("9"))
+
+			}
+		})
+
+	})
+
+	Context("Validate package distribution packages", func() {
+		It("should return RHEL8 packages", func() {
+			for _, d := range distri8 {
+				Expect(config.RHEL8).To(Equal(config.DistributionsPackages[d]))
+			}
+		})
+		It("should return RHEL8X packages", func() {
+			for _, d := range distri8X {
+				Expect(config.RHEL8X).To(Equal(config.DistributionsPackages[d]))
+			}
+		})
+		It("should return RHEL9 packages", func() {
+			for _, d := range distri9 {
+				Expect(config.RHEL90).To(Equal(config.DistributionsPackages[d]))
+			}
+		})
+
+	})
+
+	Context("Validate all supported versions", func() {
+		It("should return same size of versions", func() {
+			Expect(len(config.DistributionsPackages)).To(Equal(len(distri8) + len(distri8X) + len(distri9)))
+		})
+	})
+})
