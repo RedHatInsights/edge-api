2401a89ba54324371dc8947078abd959480a28bd
Og B. Maciel <omaciel@ogmaciel.com>
2022-11-18 10:18:41 -0500

    Make sure to use same Go version for builds.

--- pr_check.sh
+++ pr_check.sh
@@ -41,6 +41,14 @@ source $CICD_ROOT/cji_smoke_test.sh
 source $CICD_ROOT/post_test_results.sh
 
 # Generate coverate report for sonarqube
-make coverage-no-fdo
+CONTAINER_NAME="edge-pr-check-$ghprbPullId"
+
+# Run coverage using same version of Go as the App
+podman run --rm -i \
+    --name $CONTAINER_NAME \
+    -v $PWD:/opt/app-root/src:z \
+    registry.access.redhat.com/ubi8/go-toolset:1.18.4-8 \
+    make coverage-no-fdo
+
 # Generate sonarqube reports
 make scan_project
