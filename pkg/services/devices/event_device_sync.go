19a4870586c834d540c8da91c89c01365e4069c0
Djebran Lezzoum <ldjebran@gmail.com>
2022-12-02 16:33:59 +0100

    devices: fixes event_device_sync_test and add more coverage.

--- pkg/services/devices/event_device_sync.go
+++ pkg/services/devices/event_device_sync.go
@@ -24,19 +24,17 @@ func (ev EventDeviceSyncHandler) Consume(ctx context.Context) {
 
 	if ev.RedHatOrgID == "" || ev.Data.RequestID == "" {
 		eventlog.WithFields(log.Fields{
-			"message":   "Malformed device sync request, required data missing",
 			"requestId": ev.Data.RequestID,
 			"orgID":     ev.RedHatOrgID,
-		})
+		}).Error("Malformed device sync request, required data missing")
 		return
 	}
 
 	if ev.RedHatOrgID != ev.Data.Identity.Identity.OrgID {
 		eventlog.WithFields(log.Fields{
-			"message":    "Malformed device sync request, required data mis match",
 			"IdentityId": ev.Data.Identity.Identity.OrgID,
 			"orgID":      ev.RedHatOrgID,
-		})
+		}).Error("Malformed device sync request, required data mismatch")
 		return
 	}
 	// get the services from the context
--- pkg/services/devices/event_device_sync_test.go
+++ pkg/services/devices/event_device_sync_test.go
@@ -1,6 +1,7 @@
 package devices_test
 
 import (
+	"bytes"
 	"context"
 	"time"
 
@@ -20,17 +21,29 @@ import (
 var _ = Describe("Event Device sync Event Test", func() {
 	var ctx context.Context
 	var mockDeviceService *mock_services.MockDeviceServiceInterface
+	var ctrl *gomock.Controller
+	var logBuffer bytes.Buffer
+	var testLog *log.Entry
+
 	BeforeEach(func() {
-		ctrl := gomock.NewController(GinkgoT())
-		defer ctrl.Finish()
+		ctrl = gomock.NewController(GinkgoT())
 		mockDeviceService = mock_services.NewMockDeviceServiceInterface(ctrl)
+		testLog = log.NewEntry(log.StandardLogger())
+		// Set the output to use our new local logBuffer
+		logBuffer = bytes.Buffer{}
+		testLog.Logger.SetOutput(&logBuffer)
 
 		ctx = context.Background()
 		ctx = dependencies.ContextWithServices(ctx, &dependencies.EdgeAPIServices{
 			DeviceService: mockDeviceService,
 		})
-		ctx = utility.ContextWithLogger(ctx, log.NewEntry(log.StandardLogger()))
+		ctx = utility.ContextWithLogger(ctx, testLog)
 	})
+
+	AfterEach(func() {
+		ctrl.Finish()
+	})
+
 	Describe("consume device sync event", func() {
 		When("device sync is requested", func() {
 			Context("DB and inventory mismatch", func() {
@@ -47,6 +60,57 @@ var _ = Describe("Event Device sync Event Test", func() {
 					event.Data = *edgePayload
 					mockDeviceService.EXPECT().SyncDevicesWithInventory(event.RedHatOrgID).Return()
 					event.Consume(ctx)
+				})
+
+				It("SyncDevicesWithInventory should not be called when RequestID is empty", func() {
+					ident, err := common.GetIdentityFromContext(ctx)
+					Expect(err).To(BeNil())
+					edgePayload := &models.EdgeBasePayload{
+						Identity:       ident,
+						LastHandleTime: time.Now().Format(time.RFC3339),
+						RequestID:      "",
+					}
+					event := &eventReq.EventDeviceSyncHandler{}
+					event.RedHatOrgID = ident.Identity.OrgID
+					event.Data = *edgePayload
+					// SyncDevicesWithInventory should not be called
+					mockDeviceService.EXPECT().SyncDevicesWithInventory(event.RedHatOrgID).Times(0)
+					event.Consume(ctx)
+					Expect(logBuffer.String()).To(ContainSubstring("Malformed device sync request, required data missing"))
+				})
+
+				It("SyncDevicesWithInventory should not be called when OrgID is empty", func() {
+					ident, err := common.GetIdentityFromContext(ctx)
+					Expect(err).To(BeNil())
+					edgePayload := &models.EdgeBasePayload{
+						Identity:       ident,
+						LastHandleTime: time.Now().Format(time.RFC3339),
+						RequestID:      faker.UUIDHyphenated(),
+					}
+					event := &eventReq.EventDeviceSyncHandler{}
+					event.RedHatOrgID = ""
+					event.Data = *edgePayload
+					// SyncDevicesWithInventory should not be called
+					mockDeviceService.EXPECT().SyncDevicesWithInventory(gomock.Any()).Times(0)
+					event.Consume(ctx)
+					Expect(logBuffer.String()).To(ContainSubstring("Malformed device sync request, required data missing"))
+				})
+
+				It("SyncDevicesWithInventory should not be called when OrgID is different from identity one", func() {
+					ident, err := common.GetIdentityFromContext(ctx)
+					Expect(err).To(BeNil())
+					edgePayload := &models.EdgeBasePayload{
+						Identity:       ident,
+						LastHandleTime: time.Now().Format(time.RFC3339),
+						RequestID:      faker.UUIDHyphenated(),
+					}
+					event := &eventReq.EventDeviceSyncHandler{}
+					event.RedHatOrgID = faker.UUIDHyphenated()
+					event.Data = *edgePayload
+					// SyncDevicesWithInventory should not be called
+					mockDeviceService.EXPECT().SyncDevicesWithInventory(gomock.Any()).Times(0)
+					event.Consume(ctx)
+					Expect(logBuffer.String()).To(ContainSubstring("Malformed device sync request, required data mismatch"))
 				})
 			})
 		})
