11716229e9c9eb96af0e3f45c2d552ca81d58ac2
acosferreia <acosferreira@gmail.com>
2022-12-02 09:53:52 -0800

    fix test expectation

--- pkg/clients/imagebuilder/client_test.go
+++ pkg/clients/imagebuilder/client_test.go
@@ -89,10 +89,16 @@ var _ = Describe("Image Builder Client Test", func() {
 			w.Header().Set("Content-Type", "application/json")
 			w.WriteHeader(http.StatusOK)
 			fmt.Fprintln(w, `{"meta":{"count":1}}`)
+			pkgName := r.URL.Query().Get("search")
+			Expect(pkgName).To(ContainSubstring("gcc-c++"))
+			parampkgName := r.URL.RawQuery
+			Expect(parampkgName).To(ContainSubstring("search=gcc-c%2B%2B"))
+
 		}))
 		defer ts.Close()
 		config.Get().ImageBuilderConfig.URL = ts.URL
 		res, err := client.SearchPackage("gcc-c++", "x86_64", "rhel-85")
+
 		Expect(err).To(BeNil())
 		Expect(res.Meta.Count).To(Equal(1))
 	})
