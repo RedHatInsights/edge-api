f7c1029264870364640e8e5c43e4a8191119c613
Og B. Maciel <omaciel@ogmaciel.com>
2022-12-08 11:59:54 -0500

    Slightly improved unittest for models.Devices.

--- pkg/models/devices_test.go
+++ pkg/models/devices_test.go
@@ -1,35 +1,41 @@
 // FIXME: golangci-lint
 // nolint:revive,typecheck
-package models_test
+package models
 
 import (
 	"testing"
 
 	"github.com/bxcodec/faker/v3"
-	. "github.com/onsi/ginkgo"
 	"github.com/redhatinsights/edge-api/pkg/db"
-	"github.com/redhatinsights/edge-api/pkg/models"
-	// . "github.com/onsi/gomega"
-	// "github.com/redhatinsights/edge-api/pkg/models"
+	"github.com/stretchr/testify/assert"
 )
 
-var _ = Describe("Devices", func() {
-
-})
-
 func TestDevicesBeforeCreate(t *testing.T) {
-	orgID := faker.UUIDHyphenated()
-	account := faker.UUIDHyphenated()
-	devices := &models.Device{
-		Name:    faker.Name(),
-		UUID:    faker.UUIDHyphenated(),
-		OrgID:   orgID,
-		Account: account,
+
+	cases := []struct {
+		Name     string
+		Input    Device
+		Expected error
+	}{
+		{
+			"Missing orgID",
+			Device{},
+			ErrOrgIDIsMandatory,
+		},
+		{
+			"Can be created",
+			Device{
+				OrgID: faker.UUIDHyphenated(),
+				UUID:  faker.UUIDHyphenated(),
+			},
+			nil,
+		},
 	}
 
-	// BeforeCreate make sure Device has orgID
-	err := devices.BeforeCreate(db.DB)
-	if err != nil {
-		t.Error("Error running BeforeCreate")
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			got := test.Input.BeforeCreate(db.DB)
+			assert.Equal(t, test.Expected, got)
+		})
 	}
 }
