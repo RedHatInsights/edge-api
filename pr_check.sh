5172a5e9939c0323a5d1267cf3d1022eb8667d23
Victor M <vmugicag@redhat.com>
2022-12-09 19:45:12 +0100

    test another image

--- pr_check.sh
+++ pr_check.sh
@@ -8,11 +8,14 @@ export PR_CHECK="true" # Only used when doing a PR check from Github.
 # Generate coverate report for sonarqube
 CONTAINER_NAME="edge-pr-check-$ghprbPullId"
 
+#MY_IMAGE='registry.access.redhat.com/ubi8/go-toolset:1.18.4-8'
+MY_IMAGE='quay.io/app-sre/golang:1.18.4'
+
 # Run coverage using same version of Go as the App
 podman run --rm -i \
     --name "$CONTAINER_NAME" \
     -v "$PWD:/usr/src:z" \
-    registry.access.redhat.com/ubi8/go-toolset:1.18.4-8 \
+    "$MY_IMAGE" \
     bash -c 'cd /usr/src && make coverage-no-fdo'
 
 # Generate sonarqube reports
