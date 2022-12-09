413bf641470fa1d3dfcc5b4bb3106b5e4f585eb4
Victor M <vmugicag@redhat.com>
2022-12-09 18:37:32 +0100

    bugfix

--- pr_check.sh
+++ pr_check.sh
@@ -13,7 +13,7 @@ podman run --rm -i \
     --name "$CONTAINER_NAME" \
     -v "$PWD:/usr/src:z" \
     registry.access.redhat.com/ubi8/go-toolset:1.18.4-8 \
-    cd /usr/src && make coverage-no-fdo
+    bash -c 'cd /usr/src && make coverage-no-fdo'
 
 # Generate sonarqube reports
 #make scan_project
