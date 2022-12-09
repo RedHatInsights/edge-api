0e9d9d023f5d5307c480ddb7d2924328e822d6f3
Victor M <vmugicag@redhat.com>
2022-12-09 20:20:06 +0100

    Fix coverage.json generation
     - force user `root` for the image generating the coverage report
     - syntax bugfix

--- pr_check.sh
+++ pr_check.sh
@@ -1,22 +1,54 @@
 #!/bin/bash
 
-#export GOROOT="/opt/go/1.17.7" # Force Jenkins to use Go 1.17.7 since we don't have 1.18 yet
-#export PATH="${GOROOT}/bin:${PATH}"
+export GOROOT="/opt/go/1.17.7" # Force Jenkins to use Go 1.17.7 since we don't have 1.18 yet
+export PATH="${GOROOT}/bin:${PATH}"
 
 export PR_CHECK="true" # Only used when doing a PR check from Github.
+
+export APP_NAME="edge"  # name of app-sre "application" folder this component lives in
+export COMPONENT_NAME="edge-api"  # name of app-sre "resourceTemplate" in deploy.yaml for this component
+export IMAGE="quay.io/cloudservices/edge-api"  # image location on quay
+
+export IQE_PLUGINS="edge"  # name of the IQE plugin for this app.
+export IQE_MARKER_EXPRESSION="edge_smoke"  # This is the value passed to pytest -m
+export IQE_FILTER_EXPRESSION=""  # This is the value passed to pytest -k
+export IQE_CJI_TIMEOUT="30m"  # This is the time to wait for smoke test to complete or fail
+
+# Install bonfire repo/initialize
+CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
+curl -s $CICD_URL/bootstrap.sh > ${WORKSPACE}/cicd_bootstrap.sh && source ${WORKSPACE}/cicd_bootstrap.sh
+
+# Build the image and push to quay
+source $CICD_ROOT/build.sh
+
+# Run the unit tests with an ephemeral db
+# source $APP_ROOT/unit_test.sh
+
+# Deploy edge to an ephemeral namespace for testing
+source $CICD_ROOT/deploy_ephemeral_env.sh
+
+# This code is to create a 'dummy' result file so Jenkins will not fail when smoke tests are disabled
+#mkdir -p $ARTIFACTS_DIR
+#cat << EOF > $ARTIFACTS_DIR/junit-dummy.xml
+#<testsuite tests="1">
+#    <testcase classname="dummy" name="dummytest"/>
+#</testsuite>
+#EOF
+
+# Run smoke tests with ClowdJobInvocation
+source $CICD_ROOT/cji_smoke_test.sh
+# Upload test results to ibutusu
+source $CICD_ROOT/post_test_results.sh
 
 # Generate coverate report for sonarqube
 CONTAINER_NAME="edge-pr-check-$ghprbPullId"
 
-#MY_IMAGE='registry.access.redhat.com/ubi8/go-toolset:1.18.4-8'
-MY_IMAGE='quay.io/app-sre/golang:1.18.4'
-
 # Run coverage using same version of Go as the App
-podman run --rm -i \
-    --name "$CONTAINER_NAME" \
-    -v "$PWD:/usr/src:z" \
-    "$MY_IMAGE" \
+podman run --user root --rm -i \
+    --name $CONTAINER_NAME \
+    -v $PWD:/usr/src:z \
+    registry.access.redhat.com/ubi8/go-toolset:1.18.4-8 \
     bash -c 'cd /usr/src && make coverage-no-fdo'
 
 # Generate sonarqube reports
-#make scan_project
+make scan_project
