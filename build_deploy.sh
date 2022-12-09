b685a19510d444517d734a3d47b343e9a1213d7a
Og B. Maciel <omaciel@ogmaciel.com>
2022-12-09 12:52:40 -0500

    Removed Jenkins Go hack.

--- build_deploy.sh
+++ build_deploy.sh
@@ -2,7 +2,6 @@
 
 set -exv
 
-export GOROOT="/opt/go/1.17.7" # Force Jenkins to use Go 1.17.7 since we don't have 1.18 yet
 export PATH="${GOROOT}/bin:${PATH}"
 
 export PR_CHECK="false" # Only used when doing a PR check from Github.
@@ -54,7 +53,7 @@ done
 podman run --rm -i \
     -v $PWD:/usr/src:z \
     registry.access.redhat.com/ubi8/go-toolset:1.18.4-8 \
-    cd /usr/src && make coverage-no-fdo
+    /bin/bash -c 'cd /usr/src && make coverage-no-fdo'
 
 # Generate sonarqube reports
 make scan_project
--- pr_check.sh
+++ pr_check.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-export GOROOT="/opt/go/1.17.7" # Force Jenkins to use Go 1.17.7 since we don't have 1.18 yet
 export PATH="${GOROOT}/bin:${PATH}"
 
 export PR_CHECK="true" # Only used when doing a PR check from Github.
@@ -48,7 +47,7 @@ podman run --rm -i \
     --name $CONTAINER_NAME \
     -v $PWD:/usr/src:z \
     registry.access.redhat.com/ubi8/go-toolset:1.18.4-8 \
-    cd /usr/src && make coverage-no-fdo
+    /bin/bash -c 'cd /usr/src && make coverage-no-fdo'
 
 # Generate sonarqube reports
 make scan_project
