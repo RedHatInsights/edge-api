7993c69baff7095c4856d4a74143c91dba105d72
Djebran Lezzoum <ldjebran@gmail.com>
2023-02-23 18:11:29 +0100

    update: Fixes update repo status on repo building failure.
    FIXES: THEEDGE-3136
    - Set status to RepoStatusError when BuildUpdateRepo fails.
    - Update negative unittest.

--- pkg/services/updates_test.go
+++ pkg/services/updates_test.go
@@ -295,11 +295,20 @@ var _ = Describe("UpdateService Basic functions", func() {
 
 			When("when build repo fail", func() {
 				It("should return error when can't build repo", func() {
-					mockRepoBuilder.EXPECT().BuildUpdateRepo(update.ID).Return(nil, errors.New("error building repo"))
+					expectedError := errors.New("error building repo")
+					mockRepoBuilder.EXPECT().BuildUpdateRepo(update.ID).Return(nil, expectedError)
 					actual, err := updateService.CreateUpdate(update.ID)
 
-					Expect(actual).To(BeNil())
 					Expect(err).To(HaveOccurred())
+					Expect(err).To(MatchError(expectedError))
+					Expect(actual).To(BeNil())
+
+					// reload update from db
+					err = db.DB.Preload("Repo").First(&update, update.ID).Error
+					Expect(err).ToNot(HaveOccurred())
+					Expect(update.Status).To(Equal(models.UpdateStatusError))
+					Expect(update.Repo).ToNot(BeNil())
+					Expect(update.Repo.Status).To(Equal(models.RepoStatusError))
 				})
 			})
 
