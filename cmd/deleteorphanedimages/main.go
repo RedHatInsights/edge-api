cf3ef197612ed0161fe1b7477db8777d72080682
Samantha <contact@samanthanguyen.me>
2023-06-14 13:50:54 -0500

    Get to a workable state

--- cmd/deleteorphanedimages/main.go
+++ cmd/deleteorphanedimages/main.go
@@ -25,41 +25,26 @@ func main() {
 	logrus.Info("Starting deletion of orphaned images...")
 	db.InitDB()
 
-	// SELECT count(images.id)
-	// FROM `image_sets` JOIN image
-	//	ON image_sets.id=images.image_set_id
-	// WHERE (
-	// 	image_sets.deleted_at IS NOT NULL AND
-	// 	images.deleted_at IS NULL
-	// )
-	// 	AND `image_sets`.`deleted_at` IS NULL
-
-	// SELECT count(images.id) FROM `image_sets` JOIN images ON image_sets.id=images.image_set_id WHERE (image_sets.deleted_at IS NOT NULL AND images.deleted_at IS NULL) AND `image_sets`.`deleted_at` IS NULL
-
 	var orphanedImages []models.Image
-	// orphanedImagesQuery := db.DB.Debug().
-	// 	Model(&models.ImageSet{}).
-	// 	Select("count(images.id)").
-	// 	Joins("JOIN images ON image_sets.id=images.image_set_id").
-	// 	Where("image_sets.deleted_at IS NOT NULL AND images.deleted_at IS NULL")
+	var count int64
+
 	r := db.DB.Debug().
+		Model(&models.ImageSet{}).
+		Joins("JOIN images ON image_sets.id=images.image_set_id").Unscoped().
+		Where("image_sets.deleted_at IS NOT NULL AND images.deleted_at IS NULL").
 		Find(&orphanedImages).
-		Joins("JOIN images ON image_sets.id=images.image_set_id").
-		Where("image_sets.deleted_at IS NOT NULL AND images.deleted_at IS NULL")
+		Count(&count)
 
 	if r.Error != nil {
 		logrus.WithError(r.Error).Error("error when retrieving images")
 		cleanupAndExit(r.Error)
 	}
 
-	//var count int64
-	// orphanedImages.Count(&count)
-	// logrus.Info("Found ", count, " orphaned images")
-
-	// if count == 0 {
-	// 	logrus.Info("No orphaned images to delete. Exiting...")
-	// 	cleanupAndExit(nil)
-	// }
+	logrus.Info("Found ", count, " orphaned images")
+	if count == 0 {
+		logrus.Info("No orphaned images to delete. Exiting...")
+		cleanupAndExit(nil)
+	}
 
 	db.DB.Delete(&orphanedImages)
 	logrus.WithFields(logrus.Fields{"count": count}).Info("finished deleting orphaned images")
