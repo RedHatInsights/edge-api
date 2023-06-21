cdcd0344908ab0033ada22e78bf65e191af7cd8f
Samantha <contact@samanthanguyen.me>
2023-06-21 16:12:03 -0500

    Add integration tests

--- cmd/deleteorphanedimages/main.go
+++ cmd/deleteorphanedimages/main.go
@@ -1,4 +1,4 @@
-package main
+package deleteorphanedimages
 
 import (
 	"errors"
@@ -10,6 +10,7 @@ import (
 	"github.com/redhatinsights/edge-api/pkg/models"
 	feature "github.com/redhatinsights/edge-api/unleash/features"
 	"github.com/sirupsen/logrus"
+	"gorm.io/gorm"
 )
 
 // FEATURE_DELETE_ORPHANED_IMAGES=1 go run cmd/deleteorphanedimages/main.go
@@ -28,12 +29,7 @@ func main() {
 	var orphanedImages []models.Image
 	var count int64
 
-	r := db.DB.Debug().
-		Model(&models.ImageSet{}).
-		Joins("JOIN images ON image_sets.id=images.image_set_id").Unscoped().
-		Where("image_sets.deleted_at IS NOT NULL AND images.deleted_at IS NULL").
-		Find(&orphanedImages).
-		Count(&count)
+	r := findOrphanedImages(db.DB, &count, &orphanedImages)
 
 	if r.Error != nil {
 		logrus.WithError(r.Error).Error("error when retrieving images")
@@ -58,4 +54,16 @@ func cleanupAndExit(err error) {
 		logrus.Exit(2)
 	}
 	logrus.Exit(0)
+}
+
+func findOrphanedImages(
+	db *gorm.DB,
+	count *int64,
+	orphanedImages *[]models.Image,
+) *gorm.DB {
+	return db.Debug().
+		Model(&models.ImageSet{}).
+		Joins("JOIN images ON image_sets.id=images.image_set_id").Unscoped().
+		Where("image_sets.deleted_at IS NOT NULL AND images.deleted_at IS NULL").
+		Find(&orphanedImages)
 }
--- cmd/deleteorphanedimages/main_test.go
+++ cmd/deleteorphanedimages/main_test.go
@@ -1,13 +1,78 @@
-package main_test
+package deleteorphanedimages
 
 import (
 	"os"
 
+	"github.com/bxcodec/faker/v3"
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/redhatinsights/edge-api/pkg/db"
+	"github.com/redhatinsights/edge-api/pkg/models"
+	feature "github.com/redhatinsights/edge-api/unleash/features"
 )
 
 var _ = Describe("Delete orphaned images", func() {
 	It("Won't do anything if the feature is disabled", func() {
-		os.Setenv("FEATURE_DELETE_ORPHANED_IMAGES", "0")
+		err := os.Setenv(feature.DeleteOrphanedImages.EnvVar, "false")
+		Expect(err).ToNot(HaveOccurred())
+	})
+
+	Context("will delete orphaned images", func() {
+		var orgID string
+		// imageset to delete
+		var imageSet1 *models.ImageSet
+		var image11 *models.Image
+		var image12 *models.Image
+
+		// imageset to not delete
+		var imageSet2 *models.ImageSet
+		var image21 *models.Image
+		var image22 *models.Image
+
+		BeforeEach(func() {
+			err := os.Setenv(feature.DeleteOrphanedImages.EnvVar, "true")
+			Expect(err).ToNot(HaveOccurred())
+
+			if orgID == "" {
+				orgID = faker.UUIDHyphenated()
+
+				// setup imageset 1 (set to delete) + images associated
+				imageSet1 = &models.ImageSet{OrgID: orgID, Name: faker.Name(), Version: 1}
+				imageSet1err := db.DB.Create(&imageSet1).Error
+				Expect(imageSet1err).ToNot(HaveOccurred())
+
+				image11 = &models.Image{Name: imageSet1.Name, ImageSetID: &imageSet1.ID, Version: 1}
+				image12 = &models.Image{Name: imageSet1.Name, ImageSetID: &imageSet1.ID, Version: 2}
+				image1err := db.DB.Create([]models.Image{*image11, *image12}).Error
+				Expect(image1err).ToNot(HaveOccurred())
+
+				// setup image 2 (set to not delete) + images associated
+				imageSet2 = &models.ImageSet{OrgID: orgID, Name: faker.Name(), Version: 2}
+				imageSet2err := db.DB.Create(&imageSet2).Error
+				Expect(imageSet2err).ToNot(HaveOccurred())
+
+				image21 = &models.Image{Name: imageSet2.Name, ImageSetID: &imageSet2.ID, Version: 1}
+				image22 = &models.Image{Name: imageSet2.Name, ImageSetID: &imageSet2.ID, Version: 2}
+				image2err := db.DB.Create([]models.Image{*image21, *image22}).Error
+				Expect(image2err).ToNot(HaveOccurred())
+			}
+		})
+
+		AfterEach(func() {
+			err := os.Setenv(feature.DeleteOrphanedImages.EnvVar, "false")
+			Expect(err).ToNot(HaveOccurred())
+		})
+
+		It("without errors", func() {
+			var orphanedImages []models.Image
+			var count int64
+
+			imageSet1Delete := db.DB.Delete(imageSet1)
+			Expect(imageSet1Delete.Error).ToNot(HaveOccurred())
+
+			query := findOrphanedImages(db.DB, &count, &orphanedImages)
+			Expect(query.Error).ToNot(HaveOccurred())
+			Expect(count).To(Equal(2))
+		})
 	})
 })
