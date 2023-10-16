312eba3e9ec562fbad09dc20b69ebd263ce22c54
Djebran Lezzoum <ldjebran@gmail.com>
2023-10-16 14:52:02 +0200

    golang: upgrade to go version 1.19.
    Upgrade to version 1.19
    - Many dependencies latest versions require at least version 1.19
    - Fix tests that use deprecated ioutils as golangci with version 1.19 is failing.
    FIXES: https://issues.redhat.com/browse/THEEDGE-3618

--- build_deploy.sh
+++ build_deploy.sh
@@ -2,7 +2,7 @@
 
 set -exv
 
-export GOROOT="/opt/go/1.17.7" # Force Jenkins to use Go 1.17.7 since we don't have 1.18 yet
+export GOROOT="/opt/go/1.19.11"
 export PATH="${GOROOT}/bin:${PATH}"
 
 export PR_CHECK="false" # Only used when doing a PR check from Github.
@@ -53,7 +53,7 @@ done
 # Run coverage using same version of Go as the App
 podman run --user root --rm -i \
     -v $PWD:/usr/src:z \
-    registry.access.redhat.com/ubi8/go-toolset:1.18.4-8 \
+    registry.access.redhat.com/ubi8/go-toolset:1.19.10-16 \
     bash -c 'cd /usr/src && make coverage-no-fdo'
 
 # Generate sonarqube reports
