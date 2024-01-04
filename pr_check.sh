fc9b07a59e75ca2ca0554c6d55a89f05ab670620
Victor M. <vmugicag@redhat.com>
2024-01-04 18:28:36 +0100

    move to cicd-tools for pr_check.sh

--- pr_check.sh
+++ pr_check.sh
@@ -16,8 +16,7 @@ export IQE_CJI_TIMEOUT="30m"  # This is the time to wait for smoke test to compl
 
 # Install bonfire repo/initialize
 WORKSPACE=${WORKSPACE:-$PWD}
-BONFIRE_REPO_BRANCH='bonfire-hotfix'
-CICD_URL="https://raw.githubusercontent.com/RedHatInsights/bonfire/${BONFIRE_REPO_BRANCH}/cicd"
+CICD_URL="https://raw.githubusercontent.com/RedHatInsights/cicd-tools/main"
 curl -s $CICD_URL/bootstrap.sh > ${WORKSPACE}/cicd_bootstrap.sh && source ${WORKSPACE}/cicd_bootstrap.sh
 
 # env vars for bonfire
