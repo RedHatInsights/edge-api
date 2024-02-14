4c285734d39007ba955219e8bc36540527f8365c
Djebran Lezzoum <ldjebran@gmail.com>
2024-02-14 20:30:57 +0100

    go-toolset: always use gotoolset latest minor version

--- pr_check.sh
+++ pr_check.sh
@@ -48,7 +48,7 @@ CONTAINER_NAME="edge-pr-check-$ghprbPullId"
 podman run --user root --rm --replace -i \
     --name $CONTAINER_NAME \
     -v $PWD:/usr/src:z \
-    registry.access.redhat.com/ubi8/go-toolset:1.20.10-3 \
+    registry.access.redhat.com/ubi8/go-toolset:1.20 \
     bash -c 'cd /usr/src && make coverage-no-fdo'
 
 # Generate sonarqube reports
